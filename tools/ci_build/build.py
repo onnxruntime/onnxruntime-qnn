onnxruntime/onnxruntime-qnn

commit 7a4731e73bb1fee0121764517086cc930a74f197

Update manylinux build scripts and GPU CUDA version from 11.0 to 11.1 (#7632)
1. Update manylinux build scripts. This will add [PEP600](https://www.python.org/dev/peps/pep-0600/)(manylinux2 tags) support. numpy has adopted this new feature, we should do the same. The old build script files were copied from https://github.com/pypa/manylinux, but they has been deleted and replaced in the upstream repo. The manylinux repo doesn't have a manylinux2014 branch anymore. So I'm removing the obsolete code, sync the files with the latest master.
2. Update GPU CUDA version from 11.0 to 11.1(after a discussion with PMs). 
3. Delete tools/ci_build/github/linux/docker/Dockerfile.manylinux2014_cuda10_2.  (Merged the content to tools/ci_build/github/linux/docker/Dockerfile.manylinux2014_cuda11)
4. Modernize the cmake code of how to locate python devel files. It was suggested in https://github.com/onnx/onnx/pull/1631 .
5. Remove `onnxruntime_MSVC_STATIC_RUNTIME` and `onnxruntime_GCC_STATIC_CPP_RUNTIME` build options. Now cmake has builtin support for it. Starting from cmake 3.15, we can use `CMAKE_MSVC_RUNTIME_LIBRARY` cmake variable to choose which MSVC runtime library we want to use. 
6. Update Ubuntu docker images that used in our CI build from Ubuntu 18.04 to Ubuntu 20.04.
7. Update GCC version in CUDA 11.1 pipelines from 8.x to 9.3.1
8. Split Linux GPU CI pipeline to two jobs: build the code on a CPU machine then run the tests on another GPU machines.  In the past we didn't test our python packages. We only tested the pre-packed files. So we didn't catch the rpath issue in CI build. 
9. Add a CentOS machine pool and test our Linux GPU build on real CentOS machines.

tools/ci_build/build.py
@@ -351,7 +351,8 @@ def parse_arguments():
     parser.add_argument(
         "--cmake_path", default="cmake", help="Path to the CMake program.")
     parser.add_argument(
-        "--ctest_path", default="ctest", help="Path to the CTest program.")
+        "--ctest_path", default="ctest", help="Path to the CTest program. It can be an empty string. If it is empty, "
+        "we will use this script driving the test programs directly.")
     parser.add_argument(
         "--skip_submodule_sync", action='store_true', help="Don't do a "
         "'git submodule update'. Makes the Update phase faster.")
@@ -530,11 +531,14 @@ def is_reduced_ops_build(args):
 
 def resolve_executable_path(command_or_path):
     """Returns the absolute path of an executable."""
-    executable_path = shutil.which(command_or_path)
-    if executable_path is None:
-        raise BuildError("Failed to resolve executable path for "
-                         "'{}'.".format(command_or_path))
-    return os.path.abspath(executable_path)
+    if command_or_path and command_or_path.strip():
+        executable_path = shutil.which(command_or_path)
+        if executable_path is None:
+            raise BuildError("Failed to resolve executable path for "
+                             "'{}'.".format(command_or_path))
+        return os.path.abspath(executable_path)
+    else:
+        return None
 
 
 def get_linux_distro():
@@ -652,6 +656,11 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         "-Donnxruntime_BUILD_WINML_TESTS=" + ("OFF" if args.skip_winml_tests else "ON"),
         "-Donnxruntime_GENERATE_TEST_REPORTS=ON",
         "-Donnxruntime_DEV_MODE=" + use_dev_mode(args),
+        # There are two ways of locating python C API header file. "find_package(PythonLibs 3.5 REQUIRED)"
+        # and "find_package(Python 3.5 COMPONENTS Development.Module)". The first one is deprecated and it
+        # depends on the "PYTHON_EXECUTABLE" variable. The second needs "Python_EXECUTABLE". Here we set both
+        # of them to get the best compatibility.
+        "-DPython_EXECUTABLE=" + sys.executable,
         "-DPYTHON_EXECUTABLE=" + sys.executable,
         "-Donnxruntime_USE_CUDA=" + ("ON" if args.use_cuda else "OFF"),
         "-Donnxruntime_CUDA_VERSION=" + (args.cuda_version if args.use_cuda else ""),
@@ -706,7 +715,6 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         "-Donnxruntime_MINIMAL_BUILD_CUSTOM_OPS=" + ("ON" if args.minimal_build and 'custom_ops' in args.minimal_build
                                                      else "OFF"),
         "-Donnxruntime_REDUCED_OPS_BUILD=" + ("ON" if is_reduced_ops_build(args) else "OFF"),
-        "-Donnxruntime_MSVC_STATIC_RUNTIME=" + ("ON" if args.enable_msvc_static_runtime else "OFF"),
         # enable pyop if it is nightly build
         "-Donnxruntime_ENABLE_LANGUAGE_INTEROP_OPS=" + ("ON" if args.enable_language_interop_ops else "OFF"),
         "-Donnxruntime_USE_DML=" + ("ON" if args.use_dml else "OFF"),
@@ -744,6 +752,17 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         "-Donnxruntime_ENABLE_WEBASSEMBLY_DEBUG_INFO=" + ("ON" if args.enable_wasm_debug_info else "OFF"),
         "-Donnxruntime_ENABLE_EAGER_MODE=" + ("ON" if args.build_eager_mode else "OFF"),
     ]
+
+    if args.enable_msvc_static_runtime:
+        cmake_args += ["-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>",
+                       "-DONNX_USE_MSVC_STATIC_RUNTIME=ON",
+                       "-Dprotobuf_MSVC_STATIC_RUNTIME=ON",
+                       "-Dgtest_force_shared_crt=OFF"]
+    else:
+        cmake_args += ["-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>DLL",
+                       "-DONNX_USE_MSVC_STATIC_RUNTIME=OFF",
+                       "-Dprotobuf_MSVC_STATIC_RUNTIME=OFF",
+                       "-Dgtest_force_shared_crt=ON"]
 
     if acl_home and os.path.exists(acl_home):
         cmake_args += ["-Donnxruntime_ACL_HOME=" + acl_home]
@@ -1405,27 +1424,38 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
         if len(dll_path_list) > 0:
             dll_path = os.pathsep.join(dll_path_list)
 
-        if ctest_path is None:
-            # Get the "Google Test Adapter" for vstest.
-            if not os.path.exists(os.path.join(cwd,
-                                               'googletestadapter.0.17.1')):
+        if not ctest_path:
+            if is_windows():
+                # Get the "Google Test Adapter" for vstest.
+                if not os.path.exists(os.path.join(cwd,
+                                                   'googletestadapter.0.17.1')):
+                    run_subprocess(
+                        ['nuget.exe', 'restore',
+                         os.path.join(source_dir, 'packages.config'),
+                         '-ConfigFile', os.path.join(source_dir, 'NuGet.config'),
+                         '-PackagesDirectory', cwd])
+                cwd2 = os.path.join(cwd, config)
+                executables = ['onnxruntime_test_all.exe']
+                if args.build_shared_lib:
+                    executables.append('onnxruntime_shared_lib_test.exe')
+                    executables.append('onnxruntime_global_thread_pools_test.exe')
+                    executables.append('onnxruntime_api_tests_without_env.exe')
                 run_subprocess(
-                    ['nuget.exe', 'restore',
-                     os.path.join(source_dir, 'packages.config'),
-                     '-ConfigFile', os.path.join(source_dir, 'NuGet.config'),
-                     '-PackagesDirectory', cwd])
-            cwd2 = os.path.join(cwd, config)
-            executables = ['onnxruntime_test_all.exe']
-            if args.build_shared_lib:
-                executables.append('onnxruntime_shared_lib_test.exe')
-                executables.append('onnxruntime_global_thread_pools_test.exe')
-            run_subprocess(
-                ['vstest.console.exe', '--parallel',
-                 '--TestAdapterPath:..\\googletestadapter.0.17.1\\build\\_common',  # noqa
-                 '/Logger:trx', '/Enablecodecoverage', '/Platform:x64',
-                 "/Settings:%s" % os.path.join(
-                     source_dir, 'cmake\\codeconv.runsettings')] + executables,
-                cwd=cwd2, dll_path=dll_path)
+                    ['vstest.console.exe', '--parallel',
+                     '--TestAdapterPath:..\\googletestadapter.0.17.1\\build\\_common',  # noqa
+                     '/Logger:trx', '/Enablecodecoverage', '/Platform:x64',
+                     "/Settings:%s" % os.path.join(
+                         source_dir, 'cmake\\codeconv.runsettings')] + executables,
+                    cwd=cwd2, dll_path=dll_path)
+            else:
+                executables = ['onnxruntime_test_all']
+                if args.build_shared_lib:
+                    executables.append('onnxruntime_shared_lib_test')
+                    executables.append('onnxruntime_global_thread_pools_test')
+                    executables.append('onnxruntime_api_tests_without_env')
+                for exe in executables:
+                    run_subprocess([os.path.join(cwd, exe)], cwd=cwd, dll_path=dll_path)
+
         else:
             ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "7200"]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
@@ -1850,6 +1880,9 @@ def main():
     if args.build_nuget and cross_compiling:
         raise BuildError('Currently nuget package creation is not supported while cross-compiling')
 
+    if args.enable_pybind and args.disable_rtti:
+        raise BuildError("Python bindings use typeid so you can't disable RTTI")
+
     if args.enable_pybind and args.disable_exceptions:
         raise BuildError('Python bindings require exceptions to be enabled.')
 
@@ -1888,6 +1921,8 @@ def main():
     configs = set(args.config)
 
     # setup paths and directories
+    # cmake_path and ctest_path can be None. For example, if a person only wants to run the tests, he/she doesn't need
+    # to have cmake/ctest.
     cmake_path = resolve_executable_path(args.cmake_path)
     ctest_path = None if args.use_vstest else resolve_executable_path(
         args.ctest_path)

tools/ci_build/github/azure-pipelines/linux-gpu-tensorrt-ci-pipeline.yml
@@ -3,13 +3,51 @@ jobs:
   pool: onnxruntime-tensorrt-linuxbuild
   variables:
     ALLOW_RELEASED_ONNX_OPSET_ONLY: '1'
-  timeoutInMinutes: 90
+  timeoutInMinutes: 120
   steps:
-    
-    - template: templates/run-docker-build-steps.yml
+    - checkout: self
+      clean: true
+      submodules: recursive
+
+    - template: templates/get-docker-image-steps.yml
       parameters:
-        # Latest TensorRT container only supports ubuntu20.04 and python 3.8
-        RunDockerBuildArgs: '-o ubuntu20.04 -d tensorrt -r $(Build.BinariesDirectory) -p 3.8 -x "--build_wheel --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=52"'
+        Dockerfile: tools/ci_build/github/linux/docker/Dockerfile.manylinux2014_cuda11_1_tensorrt7_2
+        Context: tools/ci_build/github/linux/docker
+        DockerBuildArgs: "--network=host --build-arg POLICY=manylinux2014 --build-arg PLATFORM=x86_64  --build-arg DEVTOOLSET_ROOTPATH=/opt/rh/devtoolset-9/root --build-arg PREPEND_PATH=/opt/rh/devtoolset-9/root/usr/bin: --build-arg LD_LIBRARY_PATH_ARG=/opt/rh/devtoolset-9/root/usr/lib64:/opt/rh/devtoolset-9/root/usr/lib:/opt/rh/devtoolset-9/root/usr/lib64/dyninst:/opt/rh/devtoolset-9/root/usr/lib/dyninst:/usr/local/lib64 --build-arg BUILD_UID=$( id -u )"
+        Repository: onnxruntimegpubuild
+    - task: CmdLine@2
+      inputs:
+        script: |
+          mkdir -p $HOME/.onnx
+          docker run --gpus all -e CC=/opt/rh/devtoolset-9/root/usr/bin/cc -e CXX=/opt/rh/devtoolset-9/root/usr/bin/c++ -e CFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all" -e CXXFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all" --rm \
+            --volume /data/onnx:/data/onnx:ro \
+            --volume $(Build.SourcesDirectory):/onnxruntime_src \
+            --volume $(Build.BinariesDirectory):/build \
+            --volume /data/models:/build/models:ro \
+            --volume $HOME/.onnx:/home/onnxruntimedev/.onnx \
+            -e ALLOW_RELEASED_ONNX_OPSET_ONLY=0 \
+            -e NIGHTLY_BUILD \
+            -e BUILD_BUILDNUMBER \
+            onnxruntimegpubuild \
+            /opt/python/cp37-cp37m/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
+              --build_dir /build --cmake_generator Ninja \
+              --config Release \
+              --skip_submodule_sync \
+              --build_shared_lib \
+              --parallel \
+              --build_wheel \
+              --enable_onnx_tests --use_cuda --cuda_version=11.1 --cuda_home=/usr/local/cuda-11.1 --cudnn_home=/usr/local/cuda-11.1 \
+              --enable_pybind --build_java --build_nodejs \
+              --use_tensorrt --tensorrt_home /usr \
+              --cmake_extra_defines CMAKE_CUDA_HOST_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/cc  PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so CMAKE_CUDA_ARCHITECTURES=52
+        workingDirectory: $(Build.SourcesDirectory)
+    - task: PublishTestResults@2
+      displayName: 'Publish unit test results'
+      inputs:
+        testResultsFiles: '**/*.results.xml'
+        searchFolder: '$(Build.BinariesDirectory)'
+        testRunTitle: 'Unit Test Run'
+      condition: succeededOrFailed()
 
     - template: templates/component-governance-component-detection-steps.yml
       parameters :

tools/ci_build/github/azure-pipelines/linux-nuphar-ci-pipeline.yml
@@ -3,7 +3,7 @@ jobs:
   parameters:
     AgentPool : 'Linux-CPU-2019'
     JobName: 'Linux_CI_Dev'
-    RunDockerBuildArgs: '-o ubuntu18.04 -d cpu -r $(Build.BinariesDirectory) -x "--enable_pybind --use_nuphar"'
+    RunDockerBuildArgs: '-o ubuntu20.04 -d cpu -r $(Build.BinariesDirectory) -x "--enable_pybind --use_nuphar"'
     DoNugetPack:  'false'
     ArtifactName: 'drop-linux'
     TimeoutInMinutes: 180

tools/ci_build/github/azure-pipelines/linux-openvino-ci-pipeline.yml
@@ -3,7 +3,7 @@ jobs:
   parameters:
     AgentPool : 'Linux-CPU-2019'
     JobName: 'Linux_CI_Dev'
-    RunDockerBuildArgs: '-o ubuntu18.04 -d openvino -v 2021.3 -r $(Build.BinariesDirectory) -x "--use_openvino CPU_FP32 --build_wheel"'
+    RunDockerBuildArgs: '-o ubuntu20.04 -d openvino -v 2021.3 -r $(Build.BinariesDirectory) -x "--use_openvino CPU_FP32 --build_wheel"'
     DoNugetPack:  'false'
     ArtifactName: 'drop-linux'
     TimeoutInMinutes: 120

tools/ci_build/github/azure-pipelines/linux-openvino-nightly-pipeline.yml
@@ -23,7 +23,7 @@ jobs:
 
     - template: templates/run-docker-build-steps.yml
       parameters:
-        RunDockerBuildArgs: '-o ubuntu18.04 -d openvino -v 2020.2 -r $(Build.BinariesDirectory) -x "--use_openvino GPU_FP32 --build_wheel"'
+        RunDockerBuildArgs: '-o ubuntu20.04 -d openvino -v 2020.2 -r $(Build.BinariesDirectory) -x "--use_openvino GPU_FP32 --build_wheel"'
 
     - template: templates/component-governance-component-detection-steps.yml
       parameters :

commit 42def53ae822c54a58a98ef4b0cd0cfb4fa48ff1

Fix training e2e pipeline (#7942)
1. Fix training e2e pipeline. The failure was caused by my recent change #7632. The fix is adding "--cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=70" to the build parameters because the machines are with V100 GPUs.
2. Simplify Nuphar pipeline. It doesn't need to install a separated ONNX version(1.5.0)
3. Fix a problem that run_dockerbuild.sh ignored OS version parameter. Now because it starts to take effect, I also set python version to the system default one(3.8 for ubuntu 20.04)

tools/ci_build/build.py
@@ -1551,11 +1551,6 @@ def nuphar_run_python_tests(build_dir, configs):
         if is_windows():
             cwd = os.path.join(cwd, config)
         dll_path = os.path.join(build_dir, config, "external", "tvm", config)
-        # install onnx for shape inference in testing Nuphar scripts
-        # this needs to happen after onnx_test_data preparation which
-        # uses onnx 1.3.0
-        run_subprocess(
-            [sys.executable, '-m', 'pip', 'install', '--user', 'onnx==1.5.0'])
         run_subprocess(
             [sys.executable, 'onnxruntime_test_python_nuphar.py'],
             cwd=cwd, dll_path=dll_path)

tools/ci_build/github/azure-pipelines/linux-gpu-tensorrt-ci-pipeline.yml
@@ -39,7 +39,7 @@ jobs:
               --enable_onnx_tests --use_cuda --cuda_version=11.1 --cuda_home=/usr/local/cuda-11.1 --cudnn_home=/usr/local/cuda-11.1 \
               --enable_pybind --build_java --build_nodejs \
               --use_tensorrt --tensorrt_home /usr \
-              --cmake_extra_defines CMAKE_CUDA_HOST_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/cc  PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so CMAKE_CUDA_ARCHITECTURES=52
+              --cmake_extra_defines CMAKE_CUDA_HOST_COMPILER=/opt/rh/devtoolset-9/root/usr/bin/cc  CMAKE_CUDA_ARCHITECTURES=52
         workingDirectory: $(Build.SourcesDirectory)
     - task: PublishTestResults@2
       displayName: 'Publish unit test results'

tools/ci_build/github/azure-pipelines/linux-multi-gpu-tensorrt-ci-pipeline.yml
@@ -4,6 +4,6 @@ jobs:
     AgentPool : 'Linux-Multi-GPU'
     JobName: 'Linux_CI_Multi_GPU_TensorRT_Dev'
     # The latest TensorRT container only supports ubuntu20.04 and python 3.8
-    RunDockerBuildArgs: '-o ubuntu20.04 -d tensorrt -r $(Build.BinariesDirectory) -p 3.8 -x "--enable_multi_device_test"'
+    RunDockerBuildArgs: '-o ubuntu20.04 -p 3.8 -d tensorrt -r $(Build.BinariesDirectory) -p 3.8 -x "--enable_multi_device_test"'
     DoNugetPack:  'false'
     ArtifactName: 'drop-linux'

tools/ci_build/github/azure-pipelines/linux-nuphar-ci-pipeline.yml
@@ -3,7 +3,7 @@ jobs:
   parameters:
     AgentPool : 'Linux-CPU-2019'
     JobName: 'Linux_CI_Dev'
-    RunDockerBuildArgs: '-o ubuntu20.04 -d cpu -r $(Build.BinariesDirectory) -x "--enable_pybind --use_nuphar"'
+    RunDockerBuildArgs: '-o ubuntu20.04 -p 3.8 -d cpu -r $(Build.BinariesDirectory) -x "--enable_pybind --use_nuphar"'
     DoNugetPack:  'false'
     ArtifactName: 'drop-linux'
     TimeoutInMinutes: 180

tools/ci_build/github/azure-pipelines/linux-openvino-ci-pipeline.yml
@@ -3,7 +3,7 @@ jobs:
   parameters:
     AgentPool : 'Linux-CPU-2019'
     JobName: 'Linux_CI_Dev'
-    RunDockerBuildArgs: '-o ubuntu20.04 -d openvino -v 2021.3 -r $(Build.BinariesDirectory) -x "--use_openvino CPU_FP32 --build_wheel"'
+    RunDockerBuildArgs: '-o ubuntu20.04 -p 3.8 -d openvino -v 2021.3 -r $(Build.BinariesDirectory) -x "--use_openvino CPU_FP32 --build_wheel"'
     DoNugetPack:  'false'
     ArtifactName: 'drop-linux'
     TimeoutInMinutes: 120

tools/ci_build/github/azure-pipelines/linux-openvino-nightly-pipeline.yml
@@ -23,7 +23,7 @@ jobs:
 
     - template: templates/run-docker-build-steps.yml
       parameters:
-        RunDockerBuildArgs: '-o ubuntu20.04 -d openvino -v 2020.2 -r $(Build.BinariesDirectory) -x "--use_openvino GPU_FP32 --build_wheel"'
+        RunDockerBuildArgs: '-o ubuntu20.04 -p 3.8 -d openvino -v 2020.2 -r $(Build.BinariesDirectory) -x "--use_openvino GPU_FP32 --build_wheel"'
 
     - template: templates/component-governance-component-detection-steps.yml
       parameters :

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-ci-pipeline.yml
@@ -7,7 +7,7 @@ jobs:
     JobName: 'Onnxruntime_Linux_GPU_Training'
     SubmoduleCheckoutMode: 'recursive'
     RunDockerBuildArgs: >
-      -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory)
+      -o ubuntu20.04 -p 3.8 -d gpu -r $(Build.BinariesDirectory)
       -x "
       --enable_training
       --config $(buildConfig)

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-distributed-e2e-test-pipeline.yml
@@ -14,13 +14,13 @@ jobs:
   - template: templates/run-docker-build-steps.yml
     parameters:
       RunDockerBuildArgs: |
-        -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory) \
+        -o ubuntu20.04 -p 3.8 -d gpu -r $(Build.BinariesDirectory) \
         -t onnxruntime_e2e_test_image \
         -x " \
           --config RelWithDebInfo \
           --enable_training \
           --update --build \
-          --build_wheel \
+          --build_wheel --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=70 \
           " \
         -m
       DisplayName: 'Build'

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-distributed-test-ci-pipeline.yml
@@ -14,7 +14,7 @@ jobs:
   - template: templates/run-docker-build-steps.yml
     parameters:
       RunDockerBuildArgs: |
-        -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory) \
+        -o ubuntu20.04 -p 3.8 -d gpu -r $(Build.BinariesDirectory) \
         -t onnxruntime_distributed_tests_image \
         -x " \
           --config RelWithDebInfo \

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-ortmodule-distributed-test-ci-pipeline.yml
@@ -14,7 +14,7 @@ jobs:
   - template: templates/run-docker-build-steps.yml
     parameters:
       RunDockerBuildArgs: |
-        -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory) \
+        -o ubuntu20.04 -p 3.8 -d gpu -r $(Build.BinariesDirectory) \
         -t onnxruntime_ortmodule_distributed_tests_image \
         -x " \
           --config RelWithDebInfo \

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-ortmodule-test-ci-pipeline.yml
@@ -14,7 +14,7 @@ jobs:
   - template: templates/run-docker-build-steps.yml
     parameters:
       RunDockerBuildArgs: |
-        -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory) \
+        -o ubuntu20.04 -p 3.8 -p 3.8 -d gpu -r $(Build.BinariesDirectory) \
         -t onnxruntime_ortmodule_tests_image \
         -x " \
           --config RelWithDebInfo \

tools/ci_build/github/azure-pipelines/orttraining-linux-gpu-perf-test-ci-pipeline.yml
@@ -16,12 +16,12 @@ jobs:
   - template: templates/run-docker-build-steps.yml
     parameters:
       RunDockerBuildArgs: >
-        -o ubuntu20.04 -d gpu -r $(Build.BinariesDirectory)
+        -o ubuntu20.04 -p 3.8 -d gpu -r $(Build.BinariesDirectory)
         -t onnxruntime_perf_test_image
         -x "
         --config RelWithDebInfo
         --enable_training
-        --update --build
+        --update --build --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=70
         "
       DisplayName: 'Build performance tests'
 

tools/ci_build/github/azure-pipelines/templates/linux-ci.yml
@@ -2,7 +2,7 @@ parameters:
   AgentPool : 'Linux-CPU-2019'
   JobName : 'Linux_CI_Dev'
   SubmoduleCheckoutMode: ''
-  RunDockerBuildArgs: '-o ubuntu20.04 -d cpu -r $(Build.BinariesDirectory) -x "--build_wheel"'
+  RunDockerBuildArgs: '-o ubuntu20.04 -p 3.8 -d cpu -r $(Build.BinariesDirectory) -x "--build_wheel"'
   DoNodejsPack: 'false'
   DoNugetPack: 'false'
   NuPackScript: ''

tools/ci_build/github/linux/docker/Dockerfile.ubuntu_gpu_training
@@ -1,4 +1,6 @@
-FROM nvidia/cuda:11.1.1-cudnn8-devel-ubuntu18.04
+ARG BASEIMAGE=nvcr.io/nvidia/cuda:11.1.1-cudnn8-devel-ubuntu18.04
+
+FROM $BASEIMAGE
 
 ARG PYTHON_VERSION=3.6
 ARG INSTALL_DEPS_EXTRA_ARGS

tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
@@ -12,11 +12,12 @@ PYTHON_VER=${PYTHON_VER:=3.5}
 # Some Edge devices only have limited disk space, use this option to exclude some package
 DEVICE_TYPE=${DEVICE_TYPE:=Normal}
 
+DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 apt-get update && apt-get install -y software-properties-common lsb-release
 
-
 OS_VERSION=$(lsb_release -r -s)
-DEBIAN_FRONTEND=noninteractive
 
 SYS_LONG_BIT=$(getconf LONG_BIT)
 
@@ -144,11 +145,11 @@ rm -rf /var/lib/apt/lists/*
 
 if [ "$SYS_LONG_BIT" = "64" ]; then
     if [ "$DEVICE_TYPE" = "Normal" ]; then
-        if [ "$OS_VERSION" = "20.04" ]; then
-            aria2c -q -d /tmp -o llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-project-11.0.0.tar.xz
-        else			
-	        aria2c -q -d /tmp -o llvm.tar.xz http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-${OS_VERSION}.tar.xz
-        fi	
-	tar --strip 1 -Jxf /tmp/llvm.tar.xz -C /usr
+	    if [ "$OS_VERSION" = "20.04" ]; then
+	        #llvm 9.0 doesn't have a release for 20.04, but the binaries for 18.04 should work well.
+            OS_VERSION="18.04"
+	    fi
+        aria2c -q -d /tmp -o llvm.tar.xz http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-${OS_VERSION}.tar.xz
+	    tar --strip 1 -Jxf /tmp/llvm.tar.xz -C /usr
     fi
 fi

tools/ci_build/github/linux/run_dockerbuild.sh
@@ -59,12 +59,6 @@ if [ $BUILD_OS = "android" ]; then
     $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
         --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER}" \
         --dockerfile $DOCKER_FILE --context .
-elif [ $BUILD_OS = "centos7" ]; then
-    IMAGE="centos7"
-    DOCKER_FILE=Dockerfile.centos
-    $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
-        --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER}" \
-        --dockerfile $DOCKER_FILE --context .
 elif [ $BUILD_OS = "yocto" ]; then
     IMAGE="arm-yocto-$YOCTO_VERSION"
     DOCKER_FILE=Dockerfile.ubuntu_for_arm
@@ -76,10 +70,9 @@ elif [ $BUILD_OS = "yocto" ]; then
     $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
         --docker-build-args="--build-arg TOOL_CHAIN=$TOOL_CHAIN_SCRIPT --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER}" \
         --dockerfile $DOCKER_FILE --context .
-else
-    if [ $BUILD_DEVICE = "gpu" ]; then
+elif [ $BUILD_DEVICE = "gpu" ]; then
         #This code path is only for training. Inferecing pipeline uses CentOS
-        IMAGE="ubuntu_gpu_training"
+        IMAGE="$BUILD_OS-gpu_training"
         INSTALL_DEPS_EXTRA_ARGS="${INSTALL_DEPS_EXTRA_ARGS} -t"
         if [[ $INSTALL_DEPS_DISTRIBUTED_SETUP = true ]]; then
             INSTALL_DEPS_EXTRA_ARGS="${INSTALL_DEPS_EXTRA_ARGS} -m"
@@ -88,27 +81,37 @@ else
             INSTALL_DEPS_EXTRA_ARGS="${INSTALL_DEPS_EXTRA_ARGS} -u"
         fi
         $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
-            --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg INSTALL_DEPS_EXTRA_ARGS=\"${INSTALL_DEPS_EXTRA_ARGS}\" --build-arg USE_CONDA=${USE_CONDA} --network=host --build-arg POLICY=manylinux2014 --build-arg PLATFORM=x86_64  --build-arg DEVTOOLSET_ROOTPATH=/opt/rh/devtoolset-9/root --build-arg PREPEND_PATH=/opt/rh/devtoolset-9/root/usr/bin: --build-arg LD_LIBRARY_PATH_ARG=/opt/rh/devtoolset-9/root/usr/lib64:/opt/rh/devtoolset-9/root/usr/lib:/opt/rh/devtoolset-9/root/usr/lib64/dyninst:/opt/rh/devtoolset-9/root/usr/lib/dyninst:/usr/local/lib64" \
+            --docker-build-args="--build-arg BASEIMAGE=nvcr.io/nvidia/cuda:11.1.1-cudnn8-devel-${BUILD_OS} --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg INSTALL_DEPS_EXTRA_ARGS=\"${INSTALL_DEPS_EXTRA_ARGS}\" --build-arg USE_CONDA=${USE_CONDA} --network=host" \
             --dockerfile Dockerfile.ubuntu_gpu_training --context .
-    elif [ $BUILD_DEVICE = "tensorrt" ]; then
+elif [ $BUILD_DEVICE = "tensorrt" ]; then
         # TensorRT container release 20.12
         IMAGE="$BUILD_OS-cuda11.1-cudnn8.0-tensorrt7.2"
         DOCKER_FILE=Dockerfile.ubuntu_tensorrt
         $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
             --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --network=host --build-arg POLICY=manylinux2014 --build-arg PLATFORM=x86_64  --build-arg DEVTOOLSET_ROOTPATH=/opt/rh/devtoolset-9/root --build-arg PREPEND_PATH=/opt/rh/devtoolset-9/root/usr/bin: --build-arg LD_LIBRARY_PATH_ARG=/opt/rh/devtoolset-9/root/usr/lib64:/opt/rh/devtoolset-9/root/usr/lib:/opt/rh/devtoolset-9/root/usr/lib64/dyninst:/opt/rh/devtoolset-9/root/usr/lib/dyninst:/usr/local/lib64" \
             --dockerfile $DOCKER_FILE --context .
-    elif [ $BUILD_DEVICE = "openvino" ]; then
+elif [ $BUILD_DEVICE = "openvino" ]; then
         IMAGE="$BUILD_OS-openvino"
         DOCKER_FILE=Dockerfile.ubuntu_openvino
         $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
             --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg OPENVINO_VERSION=${OPENVINO_VERSION}" \
             --dockerfile $DOCKER_FILE --context .
-    else
+else
         IMAGE="$BUILD_OS"
+		IMAGE_OS_VERSION=""
+		if [ $BUILD_OS = "ubuntu18.04" ]; then
+		   IMAGE_OS_VERSION="18.04"
+		   PYTHON_VER="3.6"
+		elif [ $BUILD_OS = "ubuntu20.04" ]; then
+		   IMAGE_OS_VERSION="20.04"
+		   PYTHON_VER="3.8"
+		else
+		   exit 1
+	    fi
+		
         $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
-                --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER}" \
+                --docker-build-args="--build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg OS_VERSION=${IMAGE_OS_VERSION}" \
                 --dockerfile Dockerfile.ubuntu --context .
-    fi
 fi
 
 if [ -v EXTRA_IMAGE_TAG ]; then