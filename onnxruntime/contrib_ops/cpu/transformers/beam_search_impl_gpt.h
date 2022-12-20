onnxruntime/onnxruntime-qnn

commit 1967ff59e9730b06d9a3968b9f6519714f82759b

[debug] allow dump node placement in transformer models (#13954)
### Description
allow dump node placement in transformer models.

onnxruntime/contrib_ops/cpu/transformers/beam_search_impl_gpt.h
@@ -248,6 +248,9 @@ Status BeamSearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fetch
     // For the first iteration use the init_run_decoder subgraph (if present)
     if (iteration_counter++ == 0 &&
         init_run_decoder_session_state_ != nullptr) {
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+      const_cast<SessionState&>(this->init_run_decoder_session_state_).IncrementGraphExecutionCounter();
+#endif
       status = utils::ExecuteSubgraph(*init_run_decoder_session_state_,
                                       *init_run_feeds_fetches_manager,
                                       feeds,
@@ -258,6 +261,9 @@ Status BeamSearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fetch
                                       this->context_.Logger(),
                                       this->ort_stream_);
     } else {
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+      const_cast<SessionState&>(this->decoder_session_state_).IncrementGraphExecutionCounter();
+#endif
       status = utils::ExecuteSubgraph(this->decoder_session_state_,
                                       feeds_fetches_manager,
                                       feeds,

onnxruntime/contrib_ops/cpu/transformers/greedy_search_impl_gpt.h
@@ -219,6 +219,9 @@ Status GreedySearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fet
     // For the first iteration use the init_run_decoder subgraph (if present)
     if (iteration_counter++ == 0 &&
         init_run_decoder_session_state_ != nullptr) {
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+      const_cast<SessionState&>(this->init_run_decoder_session_state_).IncrementGraphExecutionCounter();
+#endif
       status = utils::ExecuteSubgraph(*init_run_decoder_session_state_,
                                       *init_run_feeds_fetches_manager,
                                       feeds,
@@ -229,6 +232,9 @@ Status GreedySearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fet
                                       this->context_.Logger(),
                                       this->ort_stream_);
     } else {
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+      const_cast<SessionState&>(this->decoder_session_state_).IncrementGraphExecutionCounter();
+#endif
       status = utils::ExecuteSubgraph(this->decoder_session_state_,
                                       feeds_fetches_manager,
                                       feeds,

onnxruntime/contrib_ops/cpu/transformers/beam_search_impl_t5.h
@@ -134,6 +134,9 @@ Status BeamSearchT5<T>::Execute(const FeedsFetchesManager& encoder_feeds_fetches
       decoder_input_ids,
       this->ort_stream_));
 
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+  const_cast<SessionState&>(this->encoder_session_state_).IncrementGraphExecutionCounter();
+#endif
   ORT_RETURN_IF_ERROR(utils::ExecuteSubgraph(this->encoder_session_state_,
                                              encoder_feeds_fetches_manager,
                                              encoder_feeds,
@@ -247,6 +250,9 @@ Status BeamSearchT5<T>::Execute(const FeedsFetchesManager& encoder_feeds_fetches
     }
 #endif
 
+#ifdef DEBUG_NODE_INPUTS_OUTPUTS
+    const_cast<SessionState&>(this->decoder_session_state_).IncrementGraphExecutionCounter();
+#endif
     status = utils::ExecuteSubgraph(this->decoder_session_state_,
                                     decoder_feeds_fetches_manager,
                                     decoder_feeds,

commit 1d1f62db2f1a5c962a3cfd5c128e3a54a8a8dcab

fix build break in transformer debug dump (#14009)
### Description

Fix build break in transformer debug dump introduced in #13954.

onnxruntime/contrib_ops/cpu/transformers/beam_search_impl_gpt.h
@@ -251,7 +251,7 @@ Status BeamSearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fetch
     if (iteration_counter++ == 0 &&
         init_run_decoder_session_state_ != nullptr) {
 #ifdef DEBUG_NODE_INPUTS_OUTPUTS
-      const_cast<SessionState&>(this->init_run_decoder_session_state_).IncrementGraphExecutionCounter();
+      const_cast<SessionState*>(this->init_run_decoder_session_state_)->IncrementGraphExecutionCounter();
 #endif
       status = utils::ExecuteSubgraph(*init_run_decoder_session_state_,
                                       *init_run_feeds_fetches_manager,

onnxruntime/contrib_ops/cpu/transformers/greedy_search_impl_gpt.h
@@ -242,7 +242,7 @@ Status GreedySearchGpt<T>::Execute(const FeedsFetchesManager* init_run_feeds_fet
     if (iteration_counter++ == 0 &&
         init_run_decoder_session_state_ != nullptr) {
 #ifdef DEBUG_NODE_INPUTS_OUTPUTS
-      const_cast<SessionState&>(this->init_run_decoder_session_state_).IncrementGraphExecutionCounter();
+      const_cast<SessionState*>(this->init_run_decoder_session_state_)->IncrementGraphExecutionCounter();
 #endif
       status = utils::ExecuteSubgraph(*init_run_decoder_session_state_,
                                       *init_run_feeds_fetches_manager,