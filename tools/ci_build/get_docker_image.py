onnxruntime/onnxruntime-qnn

commit 426d39c2d6ea036d3f109866a6957a1c72cb8280

detach patch manylinux from get_docker_image (#14958)
### Description
Make patch manylinux one single step.


### Motivation and Context
If we want to use hash of docker-related files as the cache key, the
files should keep consistent before and after docker build.
And changes in generated build_scripts should trigger rebuilding the
image as well.

tools/ci_build/get_docker_image.py
@@ -13,10 +13,7 @@ from logger import get_logger
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 REPO_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
-
 sys.path.append(os.path.join(REPO_DIR, "tools", "python"))
-
-
 from util import run  # noqa: E402
 
 log = get_logger("get_docker_image")
@@ -74,28 +71,9 @@ def main():
     # The docker file may provide a special deps.txt in its docker context dir and uses that one.
     # Otherwise, copy a generic one from this repo's cmake dir.
     if not dst_deps_file.exists():
-        log.info("Copy deps.txt to : {}".format(str(dst_deps_file)))
+        log.info(f"Copy deps.txt to : {dst_deps_file}")
         shutil.copyfile(Path(REPO_DIR) / "cmake" / "deps.txt", str(dst_deps_file))
 
-    if "manylinux" in args.dockerfile:
-        manylinux_build_scripts_folder = Path(args.manylinux_src) / "docker" / "build_scripts"
-        dest = Path(args.context) / "build_scripts"
-        if dest.exists():
-            log.info("Deleting: {}".format(str(dest)))
-            shutil.rmtree(str(dest))
-
-        shutil.copytree(str(manylinux_build_scripts_folder), str(dest))
-        src_entrypoint_file = str(Path(args.manylinux_src) / "docker" / "manylinux-entrypoint")
-        dst_entrypoint_file = str(Path(args.context) / "manylinux-entrypoint")
-        shutil.copyfile(src_entrypoint_file, dst_entrypoint_file)
-        shutil.copymode(src_entrypoint_file, dst_entrypoint_file)
-        run(
-            "patch",
-            "-p1",
-            "-i",
-            str((Path(SCRIPT_DIR) / "github" / "linux" / "docker" / "manylinux.patch").resolve()),
-            cwd=str(dest),
-        )
     if use_container_registry:
         run(
             args.docker_path,

tools/ci_build/github/azure-pipelines/templates/get-docker-image-steps.yml
@@ -15,26 +15,33 @@ parameters:
   default: true
 - name: ScriptName
   type: string
-  default: "tools/ci_build/get_docker_image.py" 
+  default: "tools/ci_build/get_docker_image.py"
 - name: UpdateDepsTxt
   type: boolean
   default: true
-  
+
 steps:
 
 - ${{ if eq(parameters.UpdateDepsTxt, true)}}:
-  - template: download-deps.yml  
+  - template: download-deps.yml
 
 - ${{ if contains(parameters.Dockerfile, 'manylinux') }}:
     - checkout: manylinux
     - script: |
-        set -e -x 
+        set -e -x
         mv manylinux onnxruntime
         mv onnxruntime ..
         cd ..
         rmdir $(Build.SourcesDirectory)
         mv onnxruntime $(Build.SourcesDirectory)
       displayName: "Move Manylinux source code to ORT folder"
+    # It makes the files in context keep consistent before and after docker build step.
+    - task: PythonScript@0
+      inputs:
+        scriptPath: $(Build.SourcesDirectory)/tools/ci_build/patch_manylinux.py
+        arguments: --dockerfile "${{ parameters.Dockerfile }}" --context "${{ parameters.Context }}"
+        pythonInterpreter: /usr/bin/python3
+      displayName: patch manylinux
 
 - ${{ if eq(parameters.UseImageCacheContainerRegistry, true) }}:
   - template: with-container-registry-steps.yml

tools/ci_build/patch_manylinux.py
@@ -1,0 +1,64 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import argparse
+import os
+import shutil
+import sys
+from pathlib import Path
+
+from logger import get_logger
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+REPO_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
+sys.path.append(os.path.join(REPO_DIR, "tools", "python"))
+from util import run  # noqa: E402
+
+log = get_logger("patch_manylinux")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Build a docker image and push it to a remote Azure Container Registry."
+        "The content in the remote registry can be used as a cache when we need to build the thing again."
+        "The user must be logged in to the container registry."
+    )
+
+    parser.add_argument("--dockerfile", default="Dockerfile", help="Path to the Dockerfile.")
+    parser.add_argument("--context", default=".", help="Path to the build context.")
+    parser.add_argument("--manylinux-src", default="manylinux", help="Path to manylinux src folder")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    log.debug(f"Dockerfile: {args.dockerfile}, context: {args.context}")
+
+    if "manylinux" in args.dockerfile:
+        manylinux_build_scripts_folder = Path(args.manylinux_src) / "docker" / "build_scripts"
+        dest = Path(args.context) / "build_scripts"
+        if dest.exists():
+            log.info(f"Deleting: {str(dest)}")
+            shutil.rmtree(str(dest))
+
+        shutil.copytree(str(manylinux_build_scripts_folder), str(dest))
+        src_entrypoint_file = str(Path(args.manylinux_src) / "docker" / "manylinux-entrypoint")
+        dst_entrypoint_file = str(Path(args.context) / "manylinux-entrypoint")
+        shutil.copyfile(src_entrypoint_file, dst_entrypoint_file)
+        shutil.copymode(src_entrypoint_file, dst_entrypoint_file)
+        run(
+            "patch",
+            "-p1",
+            "-i",
+            str((Path(SCRIPT_DIR) / "github" / "linux" / "docker" / "manylinux.patch").resolve()),
+            cwd=str(dest),
+        )
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

commit 2355bdf3f84672e292877ca32d4d43e41ea80a5f

[Fix] Error in Linux_Packaging_combined_GPU of nuget packaing pipeline (#15060)
### Description


### Motivation and Context
It caused by the #14958, in the nuget packaging pipeline, it calls
get_docker_image.py directly rather than by get-docker-image-steps.yml.
Considering the difference, one parameter is added for compatibility.


### Test Link

https://dev.azure.com/aiinfra/Lotus/_build/results?buildId=288042&view=logs&j=505ca2b7-596d-550d-8417-9b1519e87977

tools/ci_build/get_docker_image.py
@@ -44,6 +44,12 @@ def parse_args():
 
     parser.add_argument("--manylinux-src", default="manylinux", help="Path to manylinux src folder")
 
+    parser.add_argument(
+        "--multiple_repos",
+        action="store_true",
+        help="used in packaging pipeline, which couldn't use get-docker-images-steps.yml",
+    )
+
     return parser.parse_args()
 
 
@@ -75,6 +81,25 @@ def main():
     if not dst_deps_file.exists():
         log.info(f"Copy deps.txt to : {dst_deps_file}")
         shutil.copyfile(Path(REPO_DIR) / "cmake" / "deps.txt", str(dst_deps_file))
+
+    if "manylinux" in args.dockerfile and args.multiple_repos:
+        manylinux_build_scripts_folder = Path(args.manylinux_src) / "docker" / "build_scripts"
+        dest = Path(args.context) / "build_scripts"
+        if dest.exists():
+            log.info("Deleting: {}".format(str(dest)))
+            shutil.rmtree(str(dest))
+        shutil.copytree(str(manylinux_build_scripts_folder), str(dest))
+        src_entrypoint_file = str(Path(args.manylinux_src) / "docker" / "manylinux-entrypoint")
+        dst_entrypoint_file = str(Path(args.context) / "manylinux-entrypoint")
+        shutil.copyfile(src_entrypoint_file, dst_entrypoint_file)
+        shutil.copymode(src_entrypoint_file, dst_entrypoint_file)
+        run(
+            "patch",
+            "-p1",
+            "-i",
+            str((Path(SCRIPT_DIR) / "github" / "linux" / "docker" / "manylinux.patch").resolve()),
+            cwd=str(dest),
+        )
 
     if use_container_registry:
         run(

tools/ci_build/github/azure-pipelines/c-api-noopenmp-packaging-pipelines.yml
@@ -294,7 +294,6 @@ jobs:
         cd $(Build.SourcesDirectory)
         mv manylinux onnxruntime
         ls
-
   - template: templates/with-container-registry-steps.yml
     parameters:
       Steps:
@@ -304,6 +303,7 @@ jobs:
             --context tools/ci_build/github/linux/docker \
             --docker-build-args "--network=host --build-arg POLICY=manylinux2014 --build-arg PLATFORM=x86_64 --build-arg DEVTOOLSET_ROOTPATH=/opt/rh/devtoolset-11/root --build-arg PREPEND_PATH=/opt/rh/devtoolset-11/root/usr/bin: --build-arg LD_LIBRARY_PATH_ARG=/opt/rh/devtoolset-11/root/usr/lib64:/opt/rh/devtoolset-11/root/usr/lib:/opt/rh/devtoolset-11/root/usr/lib64/dyninst:/opt/rh/devtoolset-11/root/usr/lib/dyninst:/usr/local/lib64 --build-arg BUILD_UID=$( id -u )" \
             --container-registry onnxruntimebuildcache \
+            --multiple_repos \
             --repository onnxruntimecuda116xtrt85build
         displayName: "Get onnxruntimecuda116xtrt85build image for tools/ci_build/github/linux/docker/Dockerfile.manylinux2014_cuda11_6_tensorrt8_5"
         workingDirectory: $(Build.SourcesDirectory)/onnxruntime