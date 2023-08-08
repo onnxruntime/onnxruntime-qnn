onnxruntime/onnxruntime-qnn

commit 4c62f1fa5a4248fb5d2aef34df679eb8dcd1f41f

Add tool to support packing mode for BERT model (#15283)
### Description
<!-- Describe your changes. -->
Add a tool to convert fused BERT like model to packing mode


### Motivation and Context
<!-- - Why is this change required? What problem does it solve?
- If it fixes an open issue, please link to the issue here. -->

onnxruntime/contrib_ops/cuda/bert/skip_layer_norm.cc
@@ -65,17 +65,20 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
   }
 
   const auto& input_dims = input->Shape().GetDims();
-  if (input_dims.size() != 3) {
+  size_t input_dims_size = input_dims.size();
+  if (input_dims_size != 3 && input_dims_size != 2) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
-                           "input is expected to have 3 dimensions, got ", input_dims.size());
+                           "input is expected to have 3 or 2 dimensions, got ", input_dims_size);
   }
+
+  int hidden_size = static_cast<int>(input_dims[input_dims_size - 1]);
 
   const auto& gamma_dims = gamma->Shape().GetDims();
   if (gamma_dims.size() != 1) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "gamma is expected to have 1 dimension, got ", gamma_dims.size());
   }
-  if (gamma_dims[0] != input_dims[2]) {
+  if (gamma_dims[0] != hidden_size) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "Last dimension of gamma and input does not match");
   }
@@ -87,7 +90,7 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
         return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                                "beta is expected to have 1 dimension, got ", beta_dims.size());
       }
-      if (beta_dims[0] != input_dims[2]) {
+      if (beta_dims[0] != hidden_size) {
         return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                                "Last dimension of beta and input does not match");
       }
@@ -100,16 +103,13 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "bias is expected to have 1 dimension, got ", bias_dims.size());
     }
-    if (bias_dims[0] != input_dims[2]) {
+    if (bias_dims[0] != hidden_size) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "Last dimension of bias and input does not match");
     }
   }
 
-  int sequence_length = gsl::narrow_cast<int>(input_dims[1]);
-  int hidden_size = gsl::narrow_cast<int>(input_dims[2]);
-  int row_count = gsl::narrow_cast<int>(input_dims[0] * sequence_length);
-
+  int row_count = gsl::narrow<int>(input->Shape().SizeToDimension(input_dims_size - 1));
   typedef typename ToCudaType<T>::MappedType CudaT;
   HostApplyLayerNorm<CudaT, float, CudaT, Simplified>(
       GetDeviceProp(),

onnxruntime/contrib_ops/cpu/skip_layer_norm.cc
@@ -44,10 +44,13 @@ Status SkipLayerNorm<T>::Compute(OpKernelContext* p_ctx) const {
   Tensor* skip_input_bias_add_output = p_ctx->Output(3, input->Shape());
 
   const auto& input_dims = input->Shape().GetDims();
-  if (input_dims.size() != 3) {
+  size_t input_dims_size = input_dims.size();
+  if (input_dims_size != 3 && input_dims_size != 2) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
-                           "input is expected to have 3 dimensions, got ", input_dims.size());
+                           "input is expected to have 3 or 2 dimensions, got ", input_dims_size);
   }
+
+  int hidden_size = static_cast<int>(input_dims[input_dims_size - 1]);
 
   if (input->Shape() != skip->Shape()) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
@@ -59,7 +62,7 @@ Status SkipLayerNorm<T>::Compute(OpKernelContext* p_ctx) const {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "gamma is expected to have 1 dimension, got ", gamma_dims.size());
   }
-  if (gamma_dims[0] != input_dims[2]) {
+  if (gamma_dims[0] != hidden_size) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "Last dimension of gamma and input does not match");
   }
@@ -70,7 +73,7 @@ Status SkipLayerNorm<T>::Compute(OpKernelContext* p_ctx) const {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "beta is expected to have 1 dimension, got ", beta_dims.size());
     }
-    if (beta_dims[0] != input_dims[2]) {
+    if (beta_dims[0] != hidden_size) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "Last dimension of beta and input does not match");
     }
@@ -82,16 +85,14 @@ Status SkipLayerNorm<T>::Compute(OpKernelContext* p_ctx) const {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "bias is expected to have 1 dimension, got ", bias_dims.size());
     }
-    if (bias_dims[0] != input_dims[2]) {
+    if (bias_dims[0] != hidden_size) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "Last dimension of bias and input does not match");
     }
   }
 
-  int64_t batch_size = input_dims[0];
-  int64_t sequence_length = input_dims[1];
-  int64_t hidden_size = input_dims[2];
-  int64_t task_count = batch_size * sequence_length;
+
+  int64_t task_count = input->Shape().SizeToDimension(input_dims_size - 1);
 
   const T* input_data = input->Data<T>();
   const T* skip_data = skip->Data<T>();

onnxruntime/contrib_ops/rocm/bert/skip_layer_norm.cc
@@ -57,17 +57,20 @@ Status SkipLayerNorm<T>::ComputeInternal(OpKernelContext* ctx) const {
   }
 
   const auto& input_dims = input->Shape().GetDims();
-  if (input_dims.size() != 3) {
+  size_t input_dims_size = input_dims.size();
+  if (input_dims_size != 3 && input_dims_size != 2) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
-                           "input is expected to have 3 dimensions, got ", input_dims.size());
+                           "input is expected to have 3 or 2 dimensions, got ", input_dims_size);
   }
+
+  int hidden_size = static_cast<int>(input_dims[input_dims_size - 1]);
 
   const auto& gamma_dims = gamma->Shape().GetDims();
   if (gamma_dims.size() != 1) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "gamma is expected to have 1 dimension, got ", gamma_dims.size());
   }
-  if (gamma_dims[0] != input_dims[2]) {
+  if (gamma_dims[0] != hidden_size) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "Last dimension of gamma and input does not match");
   }
@@ -78,7 +81,7 @@ Status SkipLayerNorm<T>::ComputeInternal(OpKernelContext* ctx) const {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "beta is expected to have 1 dimension, got ", beta_dims.size());
     }
-    if (beta_dims[0] != input_dims[2]) {
+    if (beta_dims[0] != hidden_size) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "Last dimension of beta and input does not match");
     }
@@ -90,15 +93,13 @@ Status SkipLayerNorm<T>::ComputeInternal(OpKernelContext* ctx) const {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "bias is expected to have 1 dimension, got ", bias_dims.size());
     }
-    if (bias_dims[0] != input_dims[2]) {
+    if (bias_dims[0] != hidden_size) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                              "Last dimension of bias and input does not match");
     }
   }
 
-  int sequence_length = static_cast<int>(input_dims[1]);
-  int hidden_size = static_cast<int>(input_dims[2]);
-  int64_t element_count = input_dims[0] * sequence_length * hidden_size;
+  int64_t element_count = input->Shape().Size();
   typedef typename ToHipType<T>::MappedType HipT;
 
   return LaunchSkipLayerNormKernel<HipT, float, HipT>(

onnxruntime/core/graph/contrib_ops/bert_defs.cc
@@ -814,14 +814,46 @@ ONNX_MS_OPERATOR_SET_SCHEMA(
     OpSchema()
         .SetDoc("Skip and Root Mean Square Layer Normalization")
         .Attr("epsilon", "The epsilon value to use to avoid division by zero.", AttributeProto::FLOAT, kDefaultSkipLayerNormEpsilon)
-        .Input(0, "input", "3D input tensor with shape (batch_size, sequence_length, hidden_size)", "T")
-        .Input(1, "skip", "3D skip tensor with shape (batch_size, sequence_length, hidden_size)", "T")
-        .Input(2, "gamma", "1D input tensor with shape (hidden_size)", "T")
-        .Input(3, "bias", "1D bias tensor with shape (hidden_size", "T", OpSchema::Optional)
-        .Output(0, "output", "3D output tensor with shape (batch_size, sequence_length, hidden_size)", "T")
-        .Output(1, "mean", "Saved mean used during training to speed up gradient computation", "U", OpSchema::Optional)
-        .Output(2, "inv_std_var", "Saved inverse standard variance used during training to speed up gradient computation.", "U", OpSchema::Optional)
-        .Output(3, "input_skip_bias_sum", "Sum of the input and skip inputs (and bias if it exists) with shape (batch_size, sequence_length, hidden_size).", "T", OpSchema::Optional)
+        .Input(0,
+               "input",
+               "3D input tensor with shape (batch_size, sequence_length, hidden_size)"
+               "Or 2D input tensor with shape (token_count, hidden_size)",
+               "T")
+        .Input(1,
+              "skip",
+              "3D input tensor with shape (batch_size, sequence_length, hidden_size)"
+               "Or 2D input tensor with shape (token_count, hidden_size)",
+               "T")
+        .Input(2,
+               "gamma",
+               "1D input tensor with shape (hidden_size)",
+               "T")
+        .Input(3,
+              "bias",
+              "1D bias tensor with shape (hidden_size",
+              "T",
+              OpSchema::Optional)
+        .Output(0,
+                "output",
+                "3D output tensor with shape (batch_size, sequence_length, hidden_size)"
+                "Or 2D output tensor with shape (token_count, hidden_size)",
+                "T")
+        .Output(1,
+                "mean",
+                "Saved mean used during training to speed up gradient computation",
+                "U",
+                OpSchema::Optional)
+        .Output(2,
+                "inv_std_var",
+                "Saved inverse standard variance used during training to speed up gradient computation.",
+                "U",
+                OpSchema::Optional)
+        .Output(3,
+                "input_skip_bias_sum",
+                "Sum of the input and skip inputs (and bias if it exists)"
+                "with shape (batch_size, sequence_length, hidden_size) or (token_count, hidden_size).",
+                "T",
+                OpSchema::Optional)
         .TypeConstraint("T", {"tensor(float)", "tensor(float16)"}, "Constrain input and output types to float or half tensors.")
         .TypeConstraint("U", {"tensor(float)"}, "Constrain mean and inv_std_var to float tensors.")
         .TypeAndShapeInferenceFunction(ONNX_NAMESPACE::propagateShapeAndTypeFromFirstInput));

onnxruntime/core/providers/dnnl/subgraph/dnnl_layernorm.cc
@@ -24,13 +24,13 @@ Layer Norm:
 
                +-----------+
 (X) ---------->+           +----------> (Y)
-               |           |                  
+               |           |
 (G) ---------->+ LayerNorm +----------> (M)
-               |           |     
-(B) ---------->+           +----------> (I) 
-               +-----------+                     
-                                                                 
-    
+               |           |
+(B) ---------->+           +----------> (I)
+               +-----------+
+
+
 
 Skip Layer Norm:
   Inputs:
@@ -56,7 +56,7 @@ Skip Layer Norm:
                                      (E) ------->+           +----------> (I)
                                                  |           |
                                                  +-----------+
-                                 
+
 Attributes (epsilon)
 */
 void DnnlLayerNorm::CreatePrimitive(DnnlSubgraphPrimitive& sp, DnnlNode& node) {
@@ -82,7 +82,7 @@ void DnnlLayerNorm::CreatePrimitive(DnnlSubgraphPrimitive& sp, DnnlNode& node) {
   // This contains the layer norm op and its parameters
   ln_components op_comps;
   if (node.OpType() == "SkipLayerNormalization") {
-    
+
     // Check if shift is available
     shift_exists = node.Input(IN_BETA).Exists();
 
@@ -258,11 +258,6 @@ void DnnlLayerNorm::ValidateDims(DnnlSubgraphPrimitive& sp, DnnlNode& node) {
   // define gamma and shift input position, depending on the operation
   int gamma_pos, shift_pos;
   if (node.OpType() == "SkipLayerNormalization") {
-    // For SkipLayerNorm the spec defines the input as a 3D tensor
-    if (input_dims_size != 3) {
-      // We support 2D arrays but the expected is 3D
-      ORT_THROW("Input tensor is expected to have 3 dimensions, got ", input_dims_size);
-    }
 
     // Get skip and evaluate
     auto skip_dims = sp.GetMemory(node.Input(IN_SKIP)).get_desc().get_dims();

onnxruntime/python/tools/symbolic_shape_infer.py
@@ -190,6 +190,9 @@ class SymbolicShapeInference:
             "Neg": self._infer_symbolic_compute_ops,
             # contrib ops:
             "Attention": self._infer_Attention,
+            "PackedAttention": self._infer_PackedAttention,
+            "RemovePadding": self._infer_RemovePadding,
+            "RestorePadding": self._infer_RestorePadding,
             "BiasGelu": self._infer_BiasGelu,
             "MultiHeadAttention": self._infer_MultiHeadAttention,
             "EmbedLayerNormalization": self._infer_EmbedLayerNormalization,
@@ -445,9 +448,12 @@ class SymbolicShapeInference:
             "LayerNormalization",
             "LongformerAttention",
             "RelativePositionBias",
+            "RemovePadding",
+            "RestorePadding",
             "SimplifiedLayerNormalization",
             "SkipLayerNormalization",
             "SkipSimplifiedLayerNormalization",
+            "PackedAttention",
             "PythonOp",
             "MultiHeadAttention",
             "GroupNorm",
@@ -2096,6 +2102,54 @@ class SymbolicShapeInference:
                             past_shape[3] = f"{past_shape[3]}+{input_shape[1]}"
                     vi = self.known_vi_[node.output[1]]
                     vi.CopyFrom(helper.make_tensor_value_info(vi.name, output_dtype, past_shape))
+
+    def _infer_PackedAttention(self, node):  # noqa: N802
+        shape = self._get_shape(node, 0)
+        shape_weights = self._get_shape(node, 1)
+        shape_bias = self._try_get_shape(node, 2)
+        if shape_bias is not None:
+            assert len(shape_bias) == 1
+        tripled_hidden_size = shape_bias[0] if shape_bias is not None else shape_weights[1]
+        if shape and len(shape) == 2:
+            qkv_hidden_sizes_attr = get_attribute(node, "qkv_hidden_sizes")
+            if qkv_hidden_sizes_attr is not None:
+                assert len(qkv_hidden_sizes_attr) == 3
+                shape[1] = int(qkv_hidden_sizes_attr[2])
+            elif isinstance(tripled_hidden_size, int):
+                shape[1] = int(tripled_hidden_size / 3)
+            output_dtype = self.known_vi_[node.input[0]].type.tensor_type.elem_type
+            vi = self.known_vi_[node.output[0]]
+            vi.CopyFrom(helper.make_tensor_value_info(node.output[0], output_dtype, shape))
+
+    def _infer_RemovePadding(self, node):  # noqa: N802
+        shape = self._get_shape(node, 0)
+        if shape and len(shape) == 3:
+            output_dtype = self.known_vi_[node.input[0]].type.tensor_type.elem_type
+            vi = self.known_vi_[node.output[0]]
+            vi.CopyFrom(helper.make_tensor_value_info(node.output[0], output_dtype, ["token_count", shape[2]]))
+
+            vi_token_offset = self.known_vi_[node.output[1]]
+            vi_token_offset.CopyFrom(
+                helper.make_tensor_value_info(node.output[1], onnx.TensorProto.INT32, [shape[0], shape[1]])
+            )
+
+            vi_cumulated_seq_len = self.known_vi_[node.output[2]]
+            vi_cumulated_seq_len.CopyFrom(
+                helper.make_tensor_value_info(node.output[2], onnx.TensorProto.INT32, ["batch_size + 1"])
+            )
+
+            vi_max_seq_len = self.known_vi_[node.output[3]]
+            vi_max_seq_len.CopyFrom(helper.make_tensor_value_info(node.output[3], onnx.TensorProto.INT32, [1]))
+
+    def _infer_RestorePadding(self, node):  # noqa: N802
+        shape_input = self._get_shape(node, 0)
+        shape_token_offset = self._get_shape(node, 1)
+        if shape_input and len(shape_input) == 2 and shape_token_offset and len(shape_token_offset) == 2:
+            output_dtype = self.known_vi_[node.input[0]].type.tensor_type.elem_type
+            vi = self.known_vi_[node.output[0]]
+
+            output_shape = [shape_token_offset[0], shape_token_offset[1], shape_input[1]]
+            vi.CopyFrom(helper.make_tensor_value_info(node.output[0], output_dtype, output_shape))
 
     def _infer_BiasGelu(self, node):  # noqa: N802
         self._propagate_shape_and_type(node)

onnxruntime/python/tools/transformers/constants.py
@@ -1,0 +1,28 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+
+class Operators:
+    ATTENTION = "Attention"
+    LAYERNORM = "LayerNormalization"
+    PACKEDATTENTION = "PackedAttention"
+    REMOVEPADDING = "RemovePadding"
+    RESTOREPADDING = "RestorePadding"
+    SKIPLAYERNORM = "SkipLayerNormalization"
+
+
+class AttentionInputIDs:
+    INPUT = 0
+    WEIGHTS = 1
+    BIAS = 2
+    MASK_INDEX = 3
+    PAST = 4
+    RELATIVE_POSITION_BIAS = 5
+    PAST_SEQUENCE_LENGTH = 6
+
+
+class AttentionOutputIDs:
+    OUTPUT = 0
+    PRESENT = 1

commit 37abc2cd350c998655d4695cff56fcec348cb8c2

Fix SkipLayerNorm for 2D input (#17014)
Fix an obvious bug:
(1) In packing mode, the input for SLN has two dimensions (introduced by
#15283): [token_count, hidden_size]. Current code of `element_count =
input_dims[0] * sequence_length * hidden_size` will use element_size =
token_count * hidden_size * hidden_size, and causes invalid memory write
in cuda kernel and ORT crash

and two minor issues:
(2) potential integer overflow in `static_cast<int>(element_count)`
(3) some dead code after `return LaunchSkipLayerNormKernel` that will
never have chance to run.

onnxruntime/contrib_ops/cuda/bert/skip_layer_norm.cc
@@ -79,9 +79,10 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
   const bool skip_broadcasted = (skip_dims[0] == 1 || skip_dims_size == 2) ? true : false;
   const int skip_size = static_cast<int>(skip_dims[skip_dims_size - 1] * skip_dims[skip_dims_size - 2]);
 
+  int row_count = gsl::narrow<int>(input->Shape().SizeToDimension(input_dims_size - 1));
+  typedef typename ToCudaType<T>::MappedType CudaT;
+
   if (strict_) {
-    int row_count = gsl::narrow<int>(input->Shape().SizeToDimension(input_dims_size - 1));
-    typedef typename ToCudaType<T>::MappedType CudaT;
     HostApplyLayerNorm<CudaT, float, CudaT, Simplified>(
         GetDeviceProp(),
         Stream(ctx),
@@ -96,33 +97,23 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
         (beta != nullptr) ? reinterpret_cast<const CudaT*>(beta->Data<T>()) : nullptr,  // beta
         reinterpret_cast<const CudaT*>(skip->Data<T>()),                                // skip or residual to add
         (bias != nullptr) ? reinterpret_cast<const CudaT*>(bias->Data<T>()) : nullptr,  // bias to add
+        skip_input_bias_add_output != nullptr ? reinterpret_cast<CudaT*>(skip_input_bias_add_output->MutableData<T>()) : nullptr);
+  } else {
+    LaunchSkipLayerNormKernel<CudaT, Simplified>(
+        Stream(ctx),
+        reinterpret_cast<CudaT*>(output->MutableData<T>()),
         skip_input_bias_add_output != nullptr ? reinterpret_cast<CudaT*>(skip_input_bias_add_output->MutableData<T>()) : nullptr,
+        reinterpret_cast<const CudaT*>(input->Data<T>()),
+        reinterpret_cast<const CudaT*>(skip->Data<T>()),
+        reinterpret_cast<const CudaT*>(gamma->Data<T>()),
+        (beta != nullptr) ? reinterpret_cast<const CudaT*>(beta->Data<T>()) : nullptr,
+        (bias != nullptr) ? reinterpret_cast<const CudaT*>(bias->Data<T>()) : nullptr,
+        epsilon_,
+        hidden_size,
+        row_count,
         skip_broadcasted,
         skip_size);
-
-    CUDA_RETURN_IF_ERROR(cudaGetLastError());
-    return Status::OK();
   }
-
-  int sequence_length = static_cast<int>(input_dims[1]);
-  int64_t element_count = input_dims[0] * sequence_length * hidden_size;
-  size_t element_size = sizeof(T);
-  typedef typename ToCudaType<T>::MappedType CudaT;
-  return LaunchSkipLayerNormKernel<CudaT, Simplified>(
-      Stream(ctx),
-      reinterpret_cast<CudaT*>(output->MutableData<T>()),
-      skip_input_bias_add_output != nullptr ? reinterpret_cast<CudaT*>(skip_input_bias_add_output->MutableData<T>()) : nullptr,
-      reinterpret_cast<const CudaT*>(input->Data<T>()),
-      reinterpret_cast<const CudaT*>(skip->Data<T>()),
-      reinterpret_cast<const CudaT*>(gamma->Data<T>()),
-      (beta != nullptr) ? reinterpret_cast<const CudaT*>(beta->Data<T>()) : nullptr,
-      (bias != nullptr) ? reinterpret_cast<const CudaT*>(bias->Data<T>()) : nullptr,
-      epsilon_,
-      hidden_size,
-      static_cast<int>(element_count),
-      element_size,
-      skip_broadcasted,
-      skip_size);
 
   CUDA_RETURN_IF_ERROR(cudaGetLastError());
   return Status::OK();

onnxruntime/contrib_ops/cuda/bert/skip_layer_norm_impl.cu
@@ -174,22 +174,18 @@ __global__ void SkipLayerNormKernelSmall(
 }
 
 template <typename T, bool Simplified>
-Status LaunchSkipLayerNormKernel(
+void LaunchSkipLayerNormKernel(
     cudaStream_t stream, T* output, T* skip_input_bias_add_output, const T* input, const T* skip, const T* gamma,
-    const T* beta, const T* bias, float epsilon, const int ld, const int element_count,
-    size_t element_size,  const bool skip_broadcasted, const int skip_size) {
-  // this must be true because n is the total size of the tensor
-
-  if (element_count == 0) {
-    return Status::OK();
+    const T* beta, const T* bias, float epsilon, int ld, int row_count, bool skip_broadcasted, int skip_size) {
+  if (row_count == 0) {
+    return;
   }
 
-  assert(element_count % ld == 0);
   bool hasBias = (bias == nullptr) ? false : true;
   bool hasSkipInputBiasAdditionOutput = (skip_input_bias_add_output == nullptr) ? false : true;
 
   const int next_size = NextSize(ld);
-  const int grid_size = element_count / ld;
+  const int grid_size = row_count;
   bool flag_vec2 =
       CanVectorized<T, 2>(output, skip_input_bias_add_output, input, skip, gamma, beta, bias, ld, next_size);
   bool flag_vec4 =
@@ -231,18 +227,14 @@ Status LaunchSkipLayerNormKernel(
 #undef LAUNCH_SKIP_LAYER_NORM_KERNEL
 #undef LAUNCH_SKIP_LAYER_NORM_KERNEL_SMALL
   }
-  return CUDA_CALL(cudaGetLastError());
 }
 
-#define SKIPLAYERNORM_IMPL(T, Simplified)                                                                 \
-  template Status LaunchSkipLayerNormKernel<T, Simplified>(cudaStream_t stream, T * output,               \
-                                                           T * skip_input_bias_add_output,                \
-                                                           const T* input, const T* skip, const T* gamma, \
-                                                           const T* beta, const T* bias, float epsilon,   \
-                                                           const int ld, const int element_count,         \
-                                                           size_t element_size,  const bool skip_broadcasted, \
-                                                           const int skip_size);
-
+#define SKIPLAYERNORM_IMPL(T, Simplified)                                                               \
+  template void LaunchSkipLayerNormKernel<T, Simplified>(cudaStream_t stream, T * output,               \
+                                                         T * skip_input_bias_add_output,                \
+                                                         const T* input, const T* skip, const T* gamma, \
+                                                         const T* beta, const T* bias, float epsilon,   \
+                                                         int ld, int row_count, bool skip_broadcasted, int skip_size);
 SKIPLAYERNORM_IMPL(float, true);
 SKIPLAYERNORM_IMPL(float, false);
 SKIPLAYERNORM_IMPL(half, true);

onnxruntime/contrib_ops/cuda/bert/skip_layer_norm_impl.h
@@ -9,7 +9,7 @@ namespace contrib {
 namespace cuda {
 
 template <typename T, bool Simplified>
-Status LaunchSkipLayerNormKernel(
+void LaunchSkipLayerNormKernel(
     cudaStream_t stream,
     T* output,                      // normalized output tensor
     T* skip_input_bias_add_output,  // sum of the input and skip (and bias if it exists) tensors output
@@ -20,10 +20,9 @@ Status LaunchSkipLayerNormKernel(
     const T* bias,                  // Layer normalization beta tensor
     float epsilon,                  // Layer normalization epsilon
     int hidden_size,                // hidden size, it is the leading dimension (ld)
-    int element_count,              // number of elements in input tensor
-    size_t element_size,
-    const bool skip_broadcasted,  // determines if broadcasting should be implemented
-    const int skip_size);         // determines size of the skip tensor
+    int row_count,                  // number of rows. That is total number of elements divided by hidden size.
+    bool skip_broadcasted,          // determines if broadcasting should be implemented
+    int skip_size);                 // determines size of the skip tensor
 
 }  // namespace cuda
 }  // namespace contrib