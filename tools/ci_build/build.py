onnxruntime/onnxruntime-qnn

commit 1ce34b0d488305684d1779bcc0b30b3cfec25352

[OpenVINO-EP] UEP v3.1 Release with OpenVINO 2021.4 (#8892)
* Add command to skip tests

* Remove support for OV_2021.3_LTS and ov_2021.1

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Removed request_id parameter from all references

request_id parameter was being used with ov_2020.3
release. Starting from 2020.4 OV release, input_name
paramater is being used instead to get the
KernelContext_GetInput.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Enabling CI Logs in the branch

* CI Commits to enable logs

* Enable CI Print

* Added Imagescaler op to the supported op's list

Fixes test_tiny_yolo_V2 opset 8 model to support
fully on OV-EP. This model is the older variation
of tiny_yolo_v2 model which has Imagescaler op.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Added ops to fully support yolov3 model

-Added changes to support yolov3 opset 10 model
fully on CPU_FP32.

-This also increases the operator coverage for GPU
hardware. There by enabling yolov3 model on GPU
with fewer subgraphs.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Enabling tiny_yolov3 model fully on CPU

->Enabled tiny_yolov3 model fully on CPU.

-> Also reduces the number of subgraphs
to infer this model on GPU

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Adding GatherND op support for CPU and GPU

->This enables yolov3_pytorch model to work
with fewer subgraphs on CPU and GPU Devices.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Fixes Albert model for ISV customer

ConvTranspose op was getting rejected
due to a condition. Fixed it.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Disabling this 4 cpp tests for openvino-ep

These unit tests are failing with special conditions
for conv_transpose op with output_shape attribute.
so disabling them for now.

Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

* Docker file changes for 2021.4-v3.1

tools/ci_build/build.py
@@ -2015,6 +2015,10 @@ def main():
     if args.use_openvino == "VAD-F_FP32":
         args.test = False
 
+    # Disabling unit tests for GPU and MYRIAD on nuget creation
+    if args.use_openvino != "CPU_FP32" and args.build_nuget:
+        args.test = False
+
     configs = set(args.config)
 
     # setup paths and directories

cmake/CMakeLists.txt
@@ -1391,13 +1391,7 @@ if(onnxruntime_USE_OPENVINO)
   endif()
 
   # Check OpenVINO version for support
-  if (${VER} MATCHES "2020.3" OR $ENV{INTEL_OPENVINO_DIR} MATCHES "2020.3")
-    set(OPENVINO_VERSION "2020.3")
-    add_definitions(-DOPENVINO_2020_3=1)
-  elseif (${VER} MATCHES "2021.1" OR $ENV{INTEL_OPENVINO_DIR} MATCHES "2021.1")
-    set(OPENVINO_VERSION "2021.1")
-    add_definitions(-DOPENVINO_2021_1=1)
-  elseif (${VER} MATCHES "2021.2" OR $ENV{INTEL_OPENVINO_DIR} MATCHES "2021.2")
+  if (${VER} MATCHES "2021.2" OR $ENV{INTEL_OPENVINO_DIR} MATCHES "2021.2")
     set(OPENVINO_VERSION "2021.2")
     add_definitions(-DOPENVINO_2021_2=1)
   elseif (${VER} MATCHES "2021.3" OR $ENV{INTEL_OPENVINO_DIR} MATCHES "2021.3")

dockerfiles/Dockerfile.openvino
@@ -5,6 +5,7 @@
 
 ARG OPENVINO_VERSION=2021.4
 
+
 # Build stage
 FROM openvino/ubuntu18_runtime:${OPENVINO_VERSION} AS builder
 
@@ -16,9 +17,6 @@ ARG DEVICE=CPU_FP32
 ARG ONNXRUNTIME_REPO=https://github.com/microsoft/onnxruntime.git
 ARG ONNXRUNTIME_BRANCH=master
 
-ENV PATH /opt/miniconda/bin:/code/cmake-3.21.0-linux-x86_64/bin:$PATH
-ENV LD_LIBRARY_PATH=/opt/miniconda/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-
 ENV InferenceEngine_DIR=${INTEL_OPENVINO_DIR}/deployment_tools/inference_engine/share
 ENV ngraph_DIR=${INTEL_OPENVINO_DIR}/deployment_tools/ngraph/cmake
 
@@ -29,7 +27,7 @@ RUN /bin/sh onnxruntime/dockerfiles/scripts/install_common_deps.sh
 RUN ln -s cmake-* cmake-dir
 RUN python3 -m pip install wheel
 ENV PATH=${WORKDIR_PATH}/cmake-dir/bin:$PATH
-RUN cd onnxruntime/cmake/external/onnx && python3 setup.py install 
+RUN pip3 install onnx
 RUN cd onnxruntime && ./build.sh --config Release --update --build --parallel --use_openvino ${DEVICE} --build_shared_lib --build_wheel 
 
 #Steps to download sources
@@ -45,7 +43,6 @@ RUN tar cvf GPL_sources.tar.gz /sources
 FROM openvino/ubuntu18_runtime:${OPENVINO_VERSION}
 
 ENV DEBIAN_FRONTEND noninteractive
-
 USER root
 COPY --from=builder /home/openvino/onnxruntime/build/Linux/Release/dist/*.whl ./
 COPY --from=builder /GPL_sources.tar.gz ./

onnxruntime/core/providers/openvino/backend_utils.cc
@@ -42,6 +42,14 @@ void DumpOnnxModelProto(const ONNX_NAMESPACE::ModelProto& model_proto, std::stri
 
 #endif
 
+bool IsCILogEnabled() {
+  const std::string env_name = onnxruntime::GetEnvironmentVar("ORT_OPENVINO_ENABLE_CI_LOG");
+  if (!env_name.empty()) {
+    return true;
+  }
+  return false;
+}
+
 bool UseCompiledNetwork() {
   const std::string env_name = onnxruntime::GetEnvironmentVar("OV_USE_COMPILED_NETWORK");
   if (!env_name.empty()) {
@@ -100,6 +108,11 @@ struct static_cast_int64 {
 std::shared_ptr<InferenceEngine::CNNNetwork>
 CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalContext& global_context, const SubGraphContext& subgraph_context, std::map<std::string, std::shared_ptr<ngraph::Node>>& const_outputs_map) {
   std::shared_ptr<ngraph::Function> ng_function;
+  // NGraph Function
+  if(IsCILogEnabled()) {
+    std::cout << "CreateNgraphFunc" << std::endl;
+  }
+
 
 #ifndef NDEBUG
   if (IsDebugEnabled()) {
@@ -107,7 +120,7 @@ CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalCont
   }
 #endif
 
-#if (defined OPENVINO_2020_3) || (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3)
+#if (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3)
   ORT_UNUSED_PARAMETER(const_outputs_map);
   std::istringstream model_stream{model_proto.SerializeAsString()};
   try {
@@ -141,7 +154,6 @@ CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalCont
     ng_function->validate_nodes_and_infer_types();
   }
 
-#if (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3) || (defined OPENVINO_2021_4)
   if (!global_context.is_wholly_supported_graph) {
     std::map<std::string, std::string> result_to_output;
     for (auto& result : ng_function->get_results()) {
@@ -159,7 +171,6 @@ CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalCont
       --index;
     }
   }
-#endif
 
   try {
     return std::make_shared<InferenceEngine::CNNNetwork>(ng_function);
@@ -207,9 +218,6 @@ void SetIODefs(const ONNX_NAMESPACE::ModelProto& model_proto,
   // Configure input & output
   // Prepare input blobs
 
-#if defined OPENVINO_2020_3
-  ORT_UNUSED_PARAMETER(const_outputs_map);
-#endif
   auto inputInfo = network->getInputsInfo();
   int input_idx = 0;
   for (auto iter = inputInfo.begin(); iter != inputInfo.end(); ++iter, ++input_idx) {
@@ -222,12 +230,10 @@ void SetIODefs(const ONNX_NAMESPACE::ModelProto& model_proto,
   auto outputInfo = network->getOutputsInfo();
   for (auto iter = outputInfo.begin(); iter != outputInfo.end(); ++iter) {
     auto output_name = iter->first;
-#if (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3) || (defined OPENVINO_2021_4)
     auto it = const_outputs_map.find(output_name);
     //Output is constant and don't need to set precision
     if (it != const_outputs_map.end())
       break;
-#endif
     auto itr = output_names.find(output_name);
     if (itr == output_names.end()) {
       ORT_THROW(log_tag + "Output Names Mismatch: " + output_name + " doesn't exist");
@@ -266,7 +272,6 @@ GetOutputTensor(Ort::CustomOpApi& ort, OrtKernelContext* context, size_t batch_s
   return output_tensor;
 }
 
-#if (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3) || (defined OPENVINO_2021_4)
 OrtValue*
 GetOutputTensor(Ort::CustomOpApi& ort, OrtKernelContext* context,
                 std::string output_name,
@@ -289,7 +294,6 @@ GetOutputTensor(Ort::CustomOpApi& ort, OrtKernelContext* context,
 
   return output_tensor;
 }
-#endif
 
 int GetFirstAvailableDevice(GlobalContext& global_context) {
   int i = 0;
@@ -314,7 +318,6 @@ int GetFirstAvailableDevice(GlobalContext& global_context) {
   return i;
 }
 
-#if (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3) || (defined OPENVINO_2021_4)
 void FillOutputsWithConstantData(Ort::CustomOpApi& ort, std::shared_ptr<ngraph::Node> node, OrtValue* out_tensor) {
   switch (node->get_element_type()) {
     case ngraph::element::Type_t::f32: {
@@ -341,9 +344,7 @@ void FillOutputsWithConstantData(Ort::CustomOpApi& ort, std::shared_ptr<ngraph::
       ORT_THROW(log_tag + "Unsupported output data type");
   }
 }
-#endif
 
-#if (defined OPENVINO_2021_1) || (defined OPENVINO_2021_2) || (defined OPENVINO_2021_3) || (defined OPENVINO_2021_4)
 template <typename T>
 void FillOutputHelper(Ort::CustomOpApi& ort, OrtValue* out_tensor, std::shared_ptr<ngraph::Node> node) {
   auto const_node = std::dynamic_pointer_cast<ngraph::op::Constant>(node);
@@ -351,9 +352,8 @@ void FillOutputHelper(Ort::CustomOpApi& ort, OrtValue* out_tensor, std::shared_p
   T* tensor_data = ort.GetTensorMutableData<T>(out_tensor);
   std::copy(res.begin(), res.end(), tensor_data);
 }
-#endif
 
-void FillInputBlob(InferenceEngine::Blob::Ptr& inputBlob, size_t request_id, size_t batch_slice_idx,
+void FillInputBlob(InferenceEngine::Blob::Ptr& inputBlob, size_t batch_slice_idx,
                    std::string input_name, Ort::CustomOpApi& ort, OrtKernelContext* context,
                    InferenceEngine::Precision precision, const SubGraphContext& subgraph_context) {
   auto minput = InferenceEngine::as<InferenceEngine::MemoryBlob>(inputBlob);
@@ -362,13 +362,7 @@ void FillInputBlob(InferenceEngine::Blob::Ptr& inputBlob, size_t request_id, siz
   auto input_data = minputHolder.as<InferenceEngine::PrecisionTrait<InferenceEngine::Precision::FP32>::value_type*>();
   size_t input_data_size = inputBlob->byteSize();
 
-#if defined OPENVINO_2020_3
-  ORT_UNUSED_PARAMETER(input_name);
-  const OrtValue* tensor = ort.KernelContext_GetInput(context, subgraph_context.input_indexes[request_id]);
-#else
-  ORT_UNUSED_PARAMETER(request_id);
   const OrtValue* tensor = ort.KernelContext_GetInput(context, subgraph_context.input_names.at(input_name));
-#endif
   auto tensor_shape = ort.GetTensorTypeAndShape(tensor);
   auto elem_type = ort.GetTensorElementType(tensor_shape);
 

onnxruntime/core/providers/openvino/backend_utils.h
@@ -29,6 +29,9 @@ const std::string log_tag = "[OpenVINO-EP] ";
 bool IsDebugEnabled();
 #endif
 
+//Internal diagnostic function. 
+bool IsCILogEnabled();
+
 bool UseCompiledNetwork();
 
 std::string GetCurrentWorkingDir();
@@ -48,7 +51,6 @@ CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalCont
 
 int GetFirstAvailableDevice(GlobalContext& global_context);
 
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
 void FillOutputsWithConstantData(Ort::CustomOpApi& ort, std::shared_ptr<ngraph::Node> node, OrtValue* out_tensor);
 
 template <typename T>
@@ -59,7 +61,6 @@ GetOutputTensor(Ort::CustomOpApi& ort, OrtKernelContext* context,
                 std::string output_name,
                 std::unordered_map<std::string, int> output_names,
                 std::shared_ptr<ngraph::Node> node);
-#endif
 
 InferenceEngine::Precision
 ConvertPrecisionONNXToOpenVINO(const ONNX_NAMESPACE::TypeProto& onnx_type, std::string device);
@@ -70,7 +71,7 @@ GetOutputTensor(Ort::CustomOpApi& ort, OrtKernelContext* context, size_t batch_s
                 std::string output_name,
                 std::unordered_map<std::string, int> output_names);
 
-void FillInputBlob(InferenceEngine::Blob::Ptr& inputBlob, size_t request_id, size_t batch_slice_idx,
+void FillInputBlob(InferenceEngine::Blob::Ptr& inputBlob, size_t batch_slice_idx,
                    std::string input_name, Ort::CustomOpApi& ort, OrtKernelContext* context,
                    InferenceEngine::Precision precision, const SubGraphContext& subgraph_context);
 

onnxruntime/core/providers/openvino/backends/basic_backend.cc
@@ -78,10 +78,8 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
   if(!openvino_ep::BackendManager::GetGlobalContext().is_wholly_supported_graph) {
     ie_cnn_network_ = CreateCNNNetwork(model_proto, global_context_, subgraph_context_, const_outputs_map_);
     SetIODefs(model_proto, ie_cnn_network_, subgraph_context_.output_names, const_outputs_map_, global_context_.device_type);
-  #if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
     if (const_outputs_map_.size() == subgraph_context_.output_names.size())
       subgraph_context_.is_constant = true;
-  #endif
 
     // Loading model to the plugin
     if (subgraph_context_.is_constant) {
@@ -116,10 +114,8 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
     if(!openvino_ep::backend_utils::UseCompiledNetwork()) {
       ie_cnn_network_ = CreateCNNNetwork(model_proto, global_context_, subgraph_context_, const_outputs_map_);
       SetIODefs(model_proto, ie_cnn_network_, subgraph_context_.output_names, const_outputs_map_, global_context_.device_type);
-    #if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
       if (const_outputs_map_.size() == subgraph_context_.output_names.size())
         subgraph_context_.is_constant = true;
-    #endif
 
       // Loading model to the plugin
       if (subgraph_context_.is_constant)
@@ -131,15 +127,6 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
       }
     #endif
       if (global_context_.device_type.find("MYRIAD") != std::string::npos) {
-    #if defined(OPENVINO_2020_3)
-        if (subgraph_context_.set_vpu_config) {
-          config["VPU_DETECT_NETWORK_BATCH"] = CONFIG_VALUE(NO);
-        }
-        if (global_context_.enable_vpu_fast_compile) {
-          config["VPU_HW_INJECT_STAGES"] = CONFIG_VALUE(NO);
-          config["VPU_COPY_OPTIMIZATION"] = CONFIG_VALUE(NO);
-        }
-    #else
         if (subgraph_context_.set_vpu_config) {
           config["MYRIAD_DETECT_NETWORK_BATCH"] = CONFIG_VALUE(NO);
         }
@@ -147,7 +134,6 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
           config["MYRIAD_HW_INJECT_STAGES"] = CONFIG_VALUE(NO);
           config["MYRIAD_COPY_OPTIMIZATION"] = CONFIG_VALUE(NO);
         }
-    #endif
     #if defined(OPENVINO_2021_4)
       //to check preprocessing inside model
       config["MYRIAD_CHECK_PREPROCESSING_INSIDE_MODEL"] = CONFIG_VALUE(NO);
@@ -185,9 +171,8 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
 void BasicBackend::StartAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext* context, std::shared_ptr<InferenceEngine::InferRequest> infer_request) {
   auto graph_input_info = exe_network_.GetInputsInfo();
 
-  size_t index = 0;
   for (auto input_info_iter = graph_input_info.begin();
-       input_info_iter != graph_input_info.end(); ++input_info_iter, ++index) {
+       input_info_iter != graph_input_info.end(); ++input_info_iter) {
     // Get OpenVINO's input buffer
     InferenceEngine::Blob::Ptr graph_input_blob;
     std::string input_name = input_info_iter->first;
@@ -201,7 +186,7 @@ void BasicBackend::StartAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext* 
     }
     auto precision = input_info_iter->second->getPrecision();
     size_t batch_slice = 0;
-    FillInputBlob(graph_input_blob, index, batch_slice, input_name, ort, context, precision, subgraph_context_);
+    FillInputBlob(graph_input_blob, batch_slice, input_name, ort, context, precision, subgraph_context_);
   }
   // Start Async inference
   try {
@@ -245,7 +230,7 @@ void BasicBackend::CompleteAsyncInference(Ort::CustomOpApi& ort, OrtKernelContex
     size_t batch_slice = 0;
     FillOutputBlob(graph_output_blob, output_tensor, ort, precision, batch_slice);
   }
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
+
   if (!const_outputs_map_.empty()) {
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
@@ -254,7 +239,6 @@ void BasicBackend::CompleteAsyncInference(Ort::CustomOpApi& ort, OrtKernelContex
       FillOutputsWithConstantData(ort, node, output_tensor);
     }
   }
-#endif
 }
 
 void BasicBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
@@ -265,16 +249,19 @@ void BasicBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
   LOGS_DEFAULT(INFO) << log_tag << "In Infer";
 
   if (subgraph_context_.is_constant) {
-#if defined(OPENVINO_2021_1)  || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
       auto node = item.second;
       auto output_tensor = GetOutputTensor(ort, context, out_name, subgraph_context_.output_names, node);
       FillOutputsWithConstantData(ort, node, output_tensor);
     }
-#endif
     // Get Output tensors
     LOGS_DEFAULT(INFO) << log_tag << "Inference successful";
+    //Enable CI Logs
+    if(IsCILogEnabled()) {
+      std::cout << "Inference successful" << std::endl;
+    }
+
   } else {
       //Requesting for an idle infer_request from a pool of infer_requests_
       std::shared_ptr<InferenceEngine::InferRequest> infer_request;
@@ -290,6 +277,11 @@ void BasicBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
   
       // Get Output tensors
       LOGS_DEFAULT(INFO) << log_tag << "Inference successful";
+      //Enable CI Logs
+      if (IsCILogEnabled()) {
+        std::cout << "Inference successful" << std::endl;
+      }
+
       //Once the inference is completed, the infer_request becomes free and is placed back into pool of infer_requests_
       inferRequestsQueue_->putIdleRequest(infer_request);
 #ifndef NDEBUG

onnxruntime/core/providers/openvino/backends/vadm_backend.cc
@@ -22,12 +22,7 @@ using WaitMode = InferenceEngine::IInferRequest::WaitMode;
 #include "../contexts.h"
 #include "../backend_utils.h"
 #include "vadm_backend.h"
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || \
-    defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
 #include <vpu/hddl_config.hpp>
-#else
-#include <vpu/hddl_plugin_config.hpp>
-#endif
 
 namespace onnxruntime {
 namespace openvino_ep {
@@ -62,10 +57,8 @@ VADMBackend::VADMBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
   }
 #endif
 
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
   if (const_outputs_map_.size() == subgraph_context_.output_names.size())
     subgraph_context_.is_constant = true;
-#endif
 
   int i = 0;
   if (subgraph_context_.is_constant)
@@ -77,11 +70,7 @@ VADMBackend::VADMBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
   if (global_context_.is_wholly_supported_graph && subgraph_context_.enable_batching) {
     for (int j = 0; j < 8; j++) {
       InferenceEngine::ExecutableNetwork exe_network;
-#if defined(OPENVINO_2020_3)
-      config[VPU_HDDL_CONFIG_KEY(DEVICE_TAG)] = global_context_.deviceTags[j];
-#else
       config[InferenceEngine::HDDL_DEVICE_TAG] = global_context_.deviceTags[j];
-#endif
       try {
         exe_network = global_context_.ie_core.LoadNetwork(*ie_cnn_network_, hw_target, config);
       } catch (const Exception& e) {
@@ -144,9 +133,8 @@ void VADMBackend::StartAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext* c
   auto infer_request = infer_requests_[infer_req_idx];
   auto graph_input_info = ie_cnn_network_->getInputsInfo();
 
-  size_t index = 0;
   for (auto input_info_iter = graph_input_info.begin();
-       input_info_iter != graph_input_info.end(); ++input_info_iter, ++index) {
+       input_info_iter != graph_input_info.end(); ++input_info_iter) {
     // Get OpenVINO's input buffer
     InferenceEngine::Blob::Ptr graph_input_blob;
     std::string input_name = input_info_iter->first;
@@ -158,7 +146,7 @@ void VADMBackend::StartAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext* c
       ORT_THROW(log_tag + " Cannot access IE Blob for input: " + input_name);
     }
     auto precision = input_info_iter->second->getPrecision();
-    FillInputBlob(graph_input_blob, index, batch_slice_idx, input_name, ort, context, precision, subgraph_context_);
+    FillInputBlob(graph_input_blob, batch_slice_idx, input_name, ort, context, precision, subgraph_context_);
   }
 
   // Start Async inference
@@ -206,7 +194,6 @@ void VADMBackend::CompleteAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext
 
     FillOutputBlob(graph_output_blob, output_tensor, ort, precision, batch_slice_idx);
   }
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
   if (!const_outputs_map_.empty()) {
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
@@ -215,7 +202,6 @@ void VADMBackend::CompleteAsyncInference(Ort::CustomOpApi& ort, OrtKernelContext
       FillOutputsWithConstantData(ort, node, output_tensor);
     }
   }
-#endif
 }
 size_t DeduceBatchSize(Ort::CustomOpApi ort, const OrtValue* input_tensor,
                        InferenceEngine::SizeVector graph_dims) {
@@ -255,14 +241,12 @@ void VADMBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
   size_t remainder_parallel_runs = batch_size % num_inf_reqs_;
 
   if (subgraph_context_.is_constant) {
-#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
       auto node = item.second;
       auto output_tensor = GetOutputTensor(ort, context, out_name, subgraph_context_.output_names, node);
       FillOutputsWithConstantData(ort, node, output_tensor);
     }
-#endif
   } else {
     // Distribute the batched inputs among available Infer Requests
     // for parallel inference.

onnxruntime/core/providers/openvino/openvino_execution_provider.cc
@@ -58,6 +58,10 @@ OpenVINOExecutionProvider::GetCapability(const GraphViewer& graph_viewer, const 
   ORT_UNUSED_PARAMETER(kernel_registries);
 
   std::vector<std::unique_ptr<ComputeCapability>> result;
+  //Enable CI Logs
+  if (!(GetEnvironmentVar("ORT_OPENVINO_ENABLE_CI_LOG").empty())) {
+    std::cout << "In the OpenVINO EP" << std::endl;
+  }
   openvino_ep::BackendManager::GetGlobalContext().onnx_model_name = graph_viewer.Name();
 #ifdef _WIN32
   std::wstring onnx_path = graph_viewer.ModelPath().ToPathString();
@@ -67,14 +71,7 @@ OpenVINOExecutionProvider::GetCapability(const GraphViewer& graph_viewer, const 
 #endif
   openvino_ep::BackendManager::GetGlobalContext().onnx_opset_version = graph_viewer.DomainToVersionMap().at(kOnnxDomain);
 
-#if defined OPENVINO_2020_3
-  result = openvino_ep::GetCapability_2020_3(graph_viewer,
-                                             openvino_ep::BackendManager::GetGlobalContext().device_type);
-#elif defined (OPENVINO_2021_1)
-  openvino_ep::GetCapability obj(graph_viewer,
-                                 openvino_ep::BackendManager::GetGlobalContext().device_type, "V_2021_1");
-  result = obj.Execute();
-#elif defined (OPENVINO_2021_2)
+#if defined (OPENVINO_2021_2)
   openvino_ep::GetCapability obj(graph_viewer,
                                  openvino_ep::BackendManager::GetGlobalContext().device_type, "V_2021_2");
   result = obj.Execute();

commit 0274f82a50bee8244f9a34129c10247f6f6ab560

Fix a bug in build.py (#19652)
### Description
Fix a bug in build.py that accidentally disabled C# tests for most
builds when "--build_nuget" is specified.

### Motivation and Context
The bug was introduced in PR #8892 .

tools/ci_build/build.py
@@ -2592,7 +2592,7 @@ def main():
         raise BuildError("Using --get-api-doc requires a single build config")
 
     # Disabling unit tests for GPU on nuget creation
-    if args.use_openvino != "CPU_FP32" and args.build_nuget:
+    if args.use_openvino and args.use_openvino != "CPU_FP32" and args.build_nuget:
         args.test = False
 
     # GDK builds don't support testing