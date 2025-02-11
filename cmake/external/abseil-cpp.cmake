onnxruntime/onnxruntime-qnn

commit 0472556555ede40e549fe4152db9ad2a8f78b100

Enable VCPKG in CI build (#23426)
### Description
1. Enable VCPKG flag in Windows CPU CI build pipelines. 
2. Increased the min supported cmake version from 3.26 to 3.28. Because
of it, drop the support for the old way of finding python by
"find_package(PythonLibs)". Therefore, in build.py we no longer set
"PYTHON_EXECUTABLE" cmake var when doing cmake configure.
3. Added "xnnpack-ep" as a feature for ORT's vcpkg config.
4. Added asset cache support for ORT's vcpkg build
5. Added VCPKG triplet files for Android build.
6. Set VCPKG triplet to "universal2-osx" if CMAKE_OSX_ARCHITECTURES was
found in cmake extra defines.
7. Removed a small piece of code in build.py, which was for support CUDA
version < 11.8.
8. Fixed an issue that CMAKE_OSX_ARCHITECTURES sometimes got specified
twice when build.py invoked cmake.
9. Added more model tests to Android build. After this change, we will
test all ONNX versions instead of just the latest one.
10. Fixed issues that are related to build.py's "--build_nuget"
parameter. Also, enable the flag in most Windows CPU CI build jobs.
11. Removed a restriction in build.py that disallowed cross-compiling
Windows ARM64 nuget package on Windows x86.
 
### Motivation and Context
Adopt vcpkg.

cmake/external/abseil-cpp.cmake
@@ -22,12 +22,13 @@ endif()
 # currently absl::lts_20240116 which affects abseil-cpp.natvis debugger
 # visualization file, that must be adjusted accordingly, unless we eliminate
 # that namespace at build time.
-FetchContent_Declare(
+onnxruntime_fetchcontent_declare(
     abseil_cpp
     URL ${DEP_URL_abseil_cpp}
     URL_HASH SHA1=${DEP_SHA1_abseil_cpp}
     PATCH_COMMAND ${ABSL_PATCH_COMMAND}
     FIND_PACKAGE_ARGS 20240722 NAMES absl
+    EXCLUDE_FROM_ALL
 )
 
 onnxruntime_fetchcontent_makeavailable(abseil_cpp)

cmake/external/pybind11.cmake
@@ -1,13 +1,12 @@
-FetchContent_Declare(
+set(PYBIND11_NOPYTHON ON CACHE BOOL "" FORCE)
+set(PYBIND11_INSTALL OFF CACHE BOOL "" FORCE)
+set(PYBIND11_TEST OFF CACHE BOOL "" FORCE)
+onnxruntime_fetchcontent_declare(
     pybind11_project
     URL ${DEP_URL_pybind11}
-	URL_HASH SHA1=${DEP_SHA1_pybind11}
-	FIND_PACKAGE_ARGS 2.6 NAMES pybind11
+    URL_HASH SHA1=${DEP_SHA1_pybind11}
+    FIND_PACKAGE_ARGS 2.13 NAMES pybind11
+    EXCLUDE_FROM_ALL
 )
 onnxruntime_fetchcontent_makeavailable(pybind11_project)
 
-if(TARGET pybind11::module)
-  set(pybind11_lib pybind11::module)
-else()
-  set(pybind11_dep pybind11::pybind11)
-endif()

cmake/external/wil.cmake
@@ -3,11 +3,12 @@
 set(WIL_BUILD_PACKAGING OFF CACHE BOOL "" FORCE)
 set(WIL_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 
-FetchContent_Declare(
+onnxruntime_fetchcontent_declare(
   microsoft_wil
   URL ${DEP_URL_microsoft_wil}
   URL_HASH SHA1=${DEP_SHA1_microsoft_wil}
   FIND_PACKAGE_ARGS NAMES wil
+  EXCLUDE_FROM_ALL
 )
 
 if(WIN32)

commit 7be7609a695d7b59daa7148dd2392a72f370bfec

Fix an installation issue related to absl (#23641)
The extra word after the
"FIND_PACKAGE_ARGS" keyword would be silently ignored by CMake. 

The issue was introduced in PR #23426 on February 5, 2025. Intel
engineers reported this issue to us on February 10, 2025. Without this
fix, you may see errors like:
```
CMake Error at cmake_install.cmake:47 (include):
  include could not find requested file:

    /home/abc/src/onnxruntime/f58/Debug/_deps/abseil_cpp-build/cmake_install.cmake
```
When running "make install" command after successfully building
onnxruntime without vcpkg.
The issue doesn't exist when vcpkg is enabled.

cmake/external/abseil-cpp.cmake
@@ -26,9 +26,9 @@ onnxruntime_fetchcontent_declare(
     abseil_cpp
     URL ${DEP_URL_abseil_cpp}
     URL_HASH SHA1=${DEP_SHA1_abseil_cpp}
+    EXCLUDE_FROM_ALL
     PATCH_COMMAND ${ABSL_PATCH_COMMAND}
     FIND_PACKAGE_ARGS 20240722 NAMES absl
-    EXCLUDE_FROM_ALL
 )
 
 onnxruntime_fetchcontent_makeavailable(abseil_cpp)

cmake/external/pybind11.cmake
@@ -5,8 +5,8 @@ onnxruntime_fetchcontent_declare(
     pybind11_project
     URL ${DEP_URL_pybind11}
     URL_HASH SHA1=${DEP_SHA1_pybind11}
-    FIND_PACKAGE_ARGS 2.13 NAMES pybind11
     EXCLUDE_FROM_ALL
+    FIND_PACKAGE_ARGS 2.13 NAMES pybind11
 )
 onnxruntime_fetchcontent_makeavailable(pybind11_project)
 

cmake/external/wil.cmake
@@ -7,8 +7,8 @@ onnxruntime_fetchcontent_declare(
   microsoft_wil
   URL ${DEP_URL_microsoft_wil}
   URL_HASH SHA1=${DEP_SHA1_microsoft_wil}
-  FIND_PACKAGE_ARGS NAMES wil
   EXCLUDE_FROM_ALL
+  FIND_PACKAGE_ARGS NAMES wil
 )
 
 if(WIN32)