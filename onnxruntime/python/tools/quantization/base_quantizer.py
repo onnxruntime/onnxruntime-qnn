1dbf30deced2811a9c335b9cd0014f00e55d959d
chuteng <chuteng@qti.qualcomm.com>
2025-05-13 16:55:39 +0800

    fix merge issue

--- onnxruntime/python/tools/quantization/base_quantizer.py
+++ onnxruntime/python/tools/quantization/base_quantizer.py
@@ -210,7 +210,6 @@ class BaseQuantizer:
             replace_upsample_with_resize(self.model)
             self.model.model.opset_import.remove(ai_onnx_domain[0])
             self.model.model.opset_import.extend([onnx.helper.make_opsetid("", 11)])
-            self.replace_upsample_with_resize()
             opset_version = 11
 
         if opset_version < 19 and self.weight_qType == onnx.TensorProto.FLOAT8E4M3FN:
@@ -225,69 +224,6 @@ class BaseQuantizer:
             opset_version = 19
 
         return opset_version
-
-    def replace_upsample_with_resize(self):
-        new_nodes = []
-        for node in self.model.model.graph.node:
-            if node.op_type == "Upsample":
-                mode = None
-                for attr in node.attribute:
-                    if attr.name == 'mode':
-                        mode = attr.s.decode('utf-8')
-                        break
-
-                scales_input = None
-                if self.model.model.opset_import[0].version > 7:
-                    scales_input = node.input[1] if len(node.input) > 1 else ''
-                    resize_inputs = [node.input[0], node.name + '_roi', scales_input]
-                else:
-                    if self.model.model.opset_import[0].version == 7:
-                        for attr in node.attribute:
-                            if attr.name == 'scales':
-                                scales_input = attr.floats
-                                break
-
-                        scales_input = np.array(list(scales_input), np.float32)
-                    else:
-                        h_scale = 1
-                        w_scale = 1
-                        for attr in node.attribute:
-                            if attr.name == 'height_scale':
-                                h_scale = attr.float
-                            elif attr.name == 'width_scale':
-                                w_scale = attr.float
-
-                    scales_tensor = onnx.helper.make_tensor(
-                        name=node.name + '_scales',
-                        data_type=onnx.TensorProto.FLOAT,
-                        dims=scales_input.shape,
-                        vals=scales_input.flatten().tolist()
-                    )
-                    self.model.model.graph.initializer.append(scales_tensor)
-
-                    resize_inputs = [node.input[0], node.name + '_roi', node.name + '_scales']
-
-                roi_tensor = onnx.helper.make_tensor(
-                    name=node.name + '_roi',
-                    data_type=onnx.TensorProto.FLOAT,
-                    dims=(len(scales_input)*2,),
-                    vals=[0]*len(scales_input)+[1]*len(scales_input)
-                )
-                self.model.model.graph.initializer.append(roi_tensor)
-
-                resize_node = onnx.helper.make_node(
-                    op_type='Resize',
-                    inputs=resize_inputs,
-                    outputs = node.output,
-                    mode=mode,
-                    nearest_mode='floor'
-                )
-                node = resize_node
-
-            new_nodes.append(node)
-
-        self.model.model.graph.ClearField('node')
-        self.model.model.graph.node.extend(new_nodes)
 
     def quantize_bias_static_impl(self, bias_name, input_scale, weight_scale, beta=1.0):
         """
