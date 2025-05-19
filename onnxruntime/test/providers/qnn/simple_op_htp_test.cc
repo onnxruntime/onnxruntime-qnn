4c18d061ea18ab613e418e24312c86564f07d329
quic-tirupath <quic_tirupath@quicinc.com>
2025-05-19 14:00:26 -0700

    [QNN EP] Add ONNX ScatterElements support
     - Translate ONNX ScatterElements as QNN's ScatterElements Op
     - Handle unsupported reduction value i.e., "min"
     - Add unit tests to verify ScatterElements Op support on HTP

--- onnxruntime/test/providers/qnn/simple_op_htp_test.cc
+++ onnxruntime/test/providers/qnn/simple_op_htp_test.cc
@@ -1106,12 +1106,64 @@ TEST_F(QnnHTPBackendTests, ScatterND_int64_int64_reduction_add) {
                      ExpectedEPNodeAssignment::All);
 }
 
+// Test ScatterElements with default attributes on HTP
+TEST_F(QnnHTPBackendTests, ScatterElements_int64_int64) {
+  std::vector<int64_t> data = {0, 1, 2, 3};
+  std::vector<int64_t> indices = {1};
+  std::vector<int64_t> updates = {10};
+  RunOpTest<int64_t>("ScatterElements",
+                     {
+                         TestInputDef<int64_t>({4}, false, std::move(data)),
+                         TestInputDef<int64_t>({1, 1}, false, std::move(indices)),
+                         TestInputDef<int64_t>({1}, false, std::move(updates)),
+                     },
+                     {},
+                     17,
+                     ExpectedEPNodeAssignment::All);
+}
+
+// Test ScatterElements with reduction ADD on HTP
+TEST_F(QnnHTPBackendTests, ScatterElements_int64_int64_reduction_add) {
+  std::vector<int64_t> data = {0, 1, 2, 3};
+  std::vector<int64_t> indices = {1};
+  std::vector<int64_t> updates = {10};
+  RunOpTest<int64_t>("ScatterElements",
+                     {
+                         TestInputDef<int64_t>({4}, false, std::move(data)),
+                         TestInputDef<int64_t>({1, 1}, false, std::move(indices)),
+                         TestInputDef<int64_t>({1}, false, std::move(updates)),
+                     },
+                     {
+                         utils::MakeAttribute("reduction", "add"),
+                     },
+                     17,
+                     ExpectedEPNodeAssignment::All);
+}
+
 // Test ScatterND with reduction Mul on HTP
 TEST_F(QnnHTPBackendTests, ScatterND_int64_int64_reduction_mul) {
   std::vector<int64_t> data = {0, 1, 2, 3};
   std::vector<int64_t> indices = {1};
   std::vector<int64_t> updates = {10};
   RunOpTest<int64_t>("ScatterND",
+                     {
+                         TestInputDef<int64_t>({4}, false, std::move(data)),
+                         TestInputDef<int64_t>({1, 1}, false, std::move(indices)),
+                         TestInputDef<int64_t>({1}, false, std::move(updates)),
+                     },
+                     {
+                         utils::MakeAttribute("reduction", "mul"),
+                     },
+                     17,
+                     ExpectedEPNodeAssignment::All);
+}
+
+// Test ScatterElements with reduction Mul on HTP
+TEST_F(QnnHTPBackendTests, ScatterElements_int64_int64_reduction_mul) {
+  std::vector<int64_t> data = {0, 1, 2, 3};
+  std::vector<int64_t> indices = {1};
+  std::vector<int64_t> updates = {10};
+  RunOpTest<int64_t>("ScatterElements",
                      {
                          TestInputDef<int64_t>({4}, false, std::move(data)),
                          TestInputDef<int64_t>({1, 1}, false, std::move(indices)),
