683be6beac03b77fa00f118215a0f881d23f1f37
kromero <kromero@qti.qualcomm.com>
2025-06-05 11:29:10 -0700

    [AISW-139654] Skip failing test on QAIRT Windows

--- onnxruntime/test/python/onnxruntime_test_python_backend.py
+++ onnxruntime/test/python/onnxruntime_test_python_backend.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 # -*- coding: UTF-8 -*-
+import platform
 import unittest
 
 import numpy as np
@@ -21,6 +22,10 @@ class TestBackend(unittest.TestCase):
         output_expected = np.array([[1.0, 4.0], [9.0, 16.0], [25.0, 36.0]], dtype=np.float32)
         np.testing.assert_allclose(output_expected, res[0], rtol=1e-05, atol=1e-08)
 
+    @unittest.skipIf(
+        platform.uname().system == "Windows" and "QNNExecutionProvider" in onnxrt.get_available_providers(),
+        "Test hangs indefinitely with QAIRT Windows 2.33+",
+    )
     def test_allocation_plan_works_with_only_execute_path_to_fetches_option(self):
         """
                (inp0)  (inp1)
