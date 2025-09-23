fbb10067ba92cb70c2d140e62202f31c10ef10d4
Mu-Chein Hsu <quic_muchhsu@quicinc.com>
2025-09-23 12:49:11 -0700

    remove unused value
    Signed-off-by: Mu-Chein Hsu <quic_muchhsu@quicinc.com>

--- onnxruntime/core/providers/qnn/builder/opbuilder/topk.cc
+++ onnxruntime/core/providers/qnn/builder/opbuilder/topk.cc
@@ -61,7 +61,6 @@ Status TopKOpBuilder::ExplictOpCheck(QnnModelWrapper& qnn_model_wrapper, const N
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "The number of top elements to retrieve must be specified as constant input.");
   }
   NodeAttrHelper node_helper(node_unit);
-  auto largest = node_helper.Get("largest", 1);
   auto sorted = node_helper.Get("sorted", 1);
   if (0 == sorted) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "QNN TopK output is always sorted");
