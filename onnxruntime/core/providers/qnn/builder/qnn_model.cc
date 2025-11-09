989943294af7d35a2897a00c8ffff464a1f44684
chunghow <chunghow@qti.qualcomm.com>
2025-11-09 15:39:37 +0800

    Improve error logging for unhelpful QNN input/output tensor setup failures

--- onnxruntime/core/providers/qnn/builder/qnn_model.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_model.cc
@@ -223,6 +223,7 @@ Status QnnModel::SetupQnnInputOutput(const logging::Logger& logger) {
 
   if (Status::OK() != result) {
     const std::string message = "Failed to setup QNN input tensors for graph: " + graph_info_->Name();
+    LOGS(logger, ERROR) << result.ErrorMessage();
     LOGS(logger, ERROR) << message;
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, message);
   }
@@ -230,6 +231,7 @@ Status QnnModel::SetupQnnInputOutput(const logging::Logger& logger) {
   result = SetupTensors(qnn_output_infos_, graph_info_->OutputTensors(), false);
   if (Status::OK() != result) {
     const std::string message = "Failed to setup QNN output tensors for graph: " + graph_info_->Name();
+    LOGS(logger, ERROR) << result.ErrorMessage();
     LOGS(logger, ERROR) << message;
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, message);
   }
