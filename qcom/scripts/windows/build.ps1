3dd60c4a099c585f74ab01d655904896700c3197
hungjuiw <hungjuiw@qti.qualcomm.com>
2026-01-29 15:45:28 +0800

    Standalone build QNN EP Library

--- qcom/scripts/windows/build.ps1
+++ qcom/scripts/windows/build.ps1
@@ -169,7 +169,6 @@ if ($TargetPyVersion -ne "")
     # Wheels only supported when we can run Python for the target arch.
     $TargetPyExe = (Join-Path (Get-PythonBinDir -Version $TargetPyVersion -Arch $Arch) "python.exe")
     $BuildWheel = $true
-    $ArchArgs += "--enable_pybind"
     $BuildVEnv = (Join-Path $BuildDir "venv-$TargetPyVersion")
     Write-Host "Building Python wheel using $TargetPyExe"
 }
@@ -181,10 +180,6 @@ else {
 if ($BuildAsX) {
     $CommonArgs += "--buildasx"
 }
-
-# The ORT build incorrectly enables use of Kleidiai when using Ninja on Windows,
-# even if ArmNN is not requested. Manually turn it off.
-$PlatformArgs = @("--no_kleidiai")
 
 if ($BuildNuget) {
     $TargetNugetDir = (Get-NugetBinDir)
