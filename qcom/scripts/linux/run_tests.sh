55d2023467b110f6e761c1dbb95968fed2b595dd
kuanyul-qti <kuanyul@qti.qualcomm.com>
2026-02-05 16:09:31 +0800

    [QNN EP] Enable Python Wheel build for ABI (#30)
    * [QNN EP] Enable Python Wheel build for ABI
    
    - Package required libraries to python wheel
    - Remove unused pybind content
    - Add sample
    - Wrap the qnn and qnn ep libraries into wheel
    - Remove unnecessary copy of onnxruntime folders
    - Rename the qnn ep library
    - Add copyright
    ---------
    
    Co-authored-by: Chun-Chih Teng (Jerry) <chuteng@qti.qualcomm.com>

--- qcom/scripts/linux/run_tests.sh
+++ qcom/scripts/linux/run_tests.sh
@@ -27,24 +27,48 @@ new_build_dir="${PWD}"
 
 sed --in-place=".bak" "s@${orig_build_dir}@${new_build_dir}@g" CTestTestfile.cmake
 
-echo "Running ctests..."
-./ctest --verbose --timeout 10800
+# TODO: We will support python wheel in linux
+# log_info "-=-=-=- Running Python tests -=-=-=-"
+# mapfile -t PYTHON_TEST_FILES < "python_test_files.txt"
 
-echo "Running Python tests..."
-mapfile -t PYTHON_TEST_FILES < "python_test_files.txt"
+# for python_file in "${PYTHON_TEST_FILES[@]}"; do
+#     if [ -f "${python_file}" ]; then
+#         # TODO: [AISW-164203] ORT test failures on Rubik Pi
+#         if [[ "${python_file}" =~ ^(onnxruntime_test_python(_compile_api|_mlops)?.py)$ ]]; then
+#             log_warn "Skipping ${python_file} due to known failures."
+#         else
+#             log_debug "Running ${python_file}..."
+#             count_errors "${python_exe}" ${python_file}
+#         fi
+#     else
+#         log_warn "Failed to find ${python_file} - may be OK on platforms which do not support Python."
+#     fi
+# done
 
-for python_file in "${PYTHON_TEST_FILES[@]}"; do
-    if [ -f "${python_file}" ]; then
-        echo "Running ${python_file}..."
-        "${python_exe}" ${python_file}
-    else
-        echo "Failed to find ${python_file} - may be OK on platforms which do not support Python."
+# if [ -d "quantization" ]; then
+#     # Quantization tests ran calling unittest directly in MSFT build.py
+#     count_errors "${python_exe}" -m unittest discover -s quantization
+# else
+#     log_warn "Failed to find directory 'quantization' - may be OK on platforms which do not support Python."
+# fi
+
+log_info "-=-=-=- Running ONNX model tests -=-=-=-=-"
+
+cd "${onnx_models_root}"
+
+declare -a model_test_runners=("run_legacy_model_test" "run_model_test")
+for runner in "${model_test_runners[@]}"; do
+    "${runner}" cpu node "${REPO_ROOT}/cmake/external/onnx/onnx/backend/test/data/node"
+
+    "${runner}" cpu float32
+
+    if [ "$(uname -m)" != "aarch64" ]; then  # TODO: [AISW-164203] ORT test failures on Rubik Pi
+        "${runner}" htp qdq
     fi
+
+    log_debug "Scrubbing old context caches"
+    find "testdata/qdq-with-context-cache" -name "*_ctx.onnx" -print -delete
+    "${runner}" htp qdq-with-context-cache
 done
 
-if [ -d "quantization" ]; then
-    # Quantization tests ran calling unittest directly in MSFT build.py
-    "${python_exe}" -m unittest discover -s quantization
-else
-    echo "Failed to find directory 'quantization' - may be OK on platforms which do not support Python."
-fi
+exit "${errors}"
