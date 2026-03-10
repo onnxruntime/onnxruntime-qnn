787431b364211791bc4f711b1130989b08c53dfd
hungjuiw <hungjuiw@qti.qualcomm.com>
2026-03-10 15:55:30 +0800

    Re-enable quickgelu_test.cc

--- cmake/onnxruntime_unittests.cmake
+++ cmake/onnxruntime_unittests.cmake
@@ -215,7 +215,6 @@ file(GLOB onnxruntime_test_framework_src CONFIGURE_DEPENDS
 list(REMOVE_ITEM onnxruntime_test_framework_src
      "${TEST_SRC_DIR}/providers/qnn/fusedmatmul_test.cc"
      "${TEST_SRC_DIR}/providers/qnn/matmulnbits_test.cc"
-     "${TEST_SRC_DIR}/providers/qnn/quickgelu_test.cc"
      "${TEST_SRC_DIR}/providers/qnn/rmsnormalization_test.cc"
      "${TEST_SRC_DIR}/providers/qnn/simplifiedlayernormalization_test.cc"
      "${TEST_SRC_DIR}/providers/qnn/qnn_node_group/lpbqgemm_fusion_test.cc"
