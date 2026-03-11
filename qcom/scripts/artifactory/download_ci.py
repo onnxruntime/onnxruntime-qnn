d707abda4f1ddd40eb59c21d00e39ac575cbb892
Jeff Kilpatrick <jkilpat@qti.qualcomm.com>
2026-03-11 16:12:02 -0700

    * Add script to download artifacts

--- qcom/scripts/artifactory/download_ci.py
+++ qcom/scripts/artifactory/download_ci.py
@@ -10,38 +10,35 @@ from artifactory import CiArtifactory, initialize_logging, valid_artifact_name
 REPO_ROOT = Path(__file__).parent.parent.parent.parent.absolute()
 
 
-class PublishCiArtifact:
-    def __init__(self, name: str, build_dir: Path, src_pattern: str) -> None:
+class DownloadCiArtifact:
+    def __init__(self, name: str, destination: Path) -> None:
         self.__name = name
-        self.__build_dir = build_dir
-        self.__src_pattern = src_pattern
+        self.__destination = destination
         self.__client = CiArtifactory()
 
     @property
-    def destination(self) -> str:
+    def source(self) -> str:
         return f"{self.__client.artifact_root}/{self.__name}/"
 
     def run(self) -> None:
-        self.__client.upload(self.__build_dir, self.__src_pattern, self.destination)
+        self.__client.download(self.source, self.__destination)
 
 
 if __name__ == "__main__":
-    initialize_logging("publish_ci.py")
+    initialize_logging("download_ci.py")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", type=valid_artifact_name, required=True, help="Artifact name")
     parser.add_argument(
-        "--src-root",
+        "--dest-root",
         type=Path,
         default=REPO_ROOT / "build",
         help="Root directory of artifacts"
     )
-    parser.add_argument("--src-pattern", type=str, required=True, help="Artifact file pattern, relative to SRC_ROOT.")
 
     args = parser.parse_args()
 
-    PublishCiArtifact(
+    DownloadCiArtifact(
         args.name,
-        args.src_root,
-        args.src_pattern,
+        args.dest_root,
     ).run()
