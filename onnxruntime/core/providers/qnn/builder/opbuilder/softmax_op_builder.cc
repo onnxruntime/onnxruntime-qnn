aad762f35be0ecee9f68a19f0ebce00d72942cd4
Badri Narayanan <mbadnara@qti.qualcomm.com>
2026-03-18 11:45:01 -0700

    [QNN EP] - Add support for Beta param for both Softmax and LogSoftmax

--- onnxruntime/core/providers/qnn/builder/opbuilder/softmax_op_builder.cc
+++ onnxruntime/core/providers/qnn/builder/opbuilder/softmax_op_builder.cc
@@ -164,10 +164,10 @@ Ort::Status SoftmaxOpBuilder::ProcessAttributesAndOutputs(QnnModelWrapper& qnn_m
   Qnn_Scalar_t axis_qnn_scalar = QNN_SCALAR_INIT;
   RETURN_IF_ERROR(ProcessAxisAttribute(qnn_model_wrapper, node_unit, axis_qnn_scalar, axis));
 
-  // Beta is a QNN-specific extension supported only for LogSoftmax (not standard ONNX Softmax).
-  const bool is_log_softmax = (op_type == "LogSoftmax");
+  // Beta is a QNN-specific parameter supported for both Softmax and LogSoftmax. Defaults to 1.0.
   OrtNodeAttrHelper node_helper(node_unit);
-  const float beta = is_log_softmax ? node_helper.Get("beta", 1.0f) : 1.0f;
+  const float beta = node_helper.Get("beta", 1.0f);
+  const std::string beta_param_key = (op_type == "LogSoftmax") ? QNN_OP_LOG_SOFTMAX_PARAM_BETA : QNN_OP_SOFTMAX_PARAM_BETA;
   Qnn_Scalar_t beta_qnn_scalar = QNN_SCALAR_INIT;
   beta_qnn_scalar.dataType = QNN_DATATYPE_FLOAT_32;
   beta_qnn_scalar.floatValue = beta;
@@ -190,11 +190,9 @@ Ort::Status SoftmaxOpBuilder::ProcessAttributesAndOutputs(QnnModelWrapper& qnn_m
     param_tensor_names.push_back(axis_param.GetParamTensorName());
     qnn_model_wrapper.AddParamWrapper(std::move(axis_param));
 
-    if (is_log_softmax) {
-      QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), QNN_OP_LOG_SOFTMAX_PARAM_BETA, beta_qnn_scalar);
-      param_tensor_names.push_back(beta_param.GetParamTensorName());
-      qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
-    }
+    QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), beta_param_key, beta_qnn_scalar);
+    param_tensor_names.push_back(beta_param.GetParamTensorName());
+    qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
 
     QnnTensorWrapper output_tensorwrapper(reshape_input_name, QNN_TENSOR_TYPE_NATIVE, output_info.qnn_data_type,
                                           output_info.quant_param.Copy(), std::vector<uint32_t>(reshape_input_shape));
@@ -232,11 +230,9 @@ Ort::Status SoftmaxOpBuilder::ProcessAttributesAndOutputs(QnnModelWrapper& qnn_m
     param_tensor_names.push_back(axis_param.GetParamTensorName());
     qnn_model_wrapper.AddParamWrapper(std::move(axis_param));
 
-    if (is_log_softmax) {
-      QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), QNN_OP_LOG_SOFTMAX_PARAM_BETA, beta_qnn_scalar);
-      param_tensor_names.push_back(beta_param.GetParamTensorName());
-      qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
-    }
+    QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), beta_param_key, beta_qnn_scalar);
+    param_tensor_names.push_back(beta_param.GetParamTensorName());
+    qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
 
     QnnTensorWrapper output_tensorwrapper(transpose_input_name, QNN_TENSOR_TYPE_NATIVE, output_info.qnn_data_type,
                                           output_info.quant_param.Copy(), std::vector<uint32_t>(transpose_input_shape));
@@ -272,11 +268,9 @@ Ort::Status SoftmaxOpBuilder::ProcessAttributesAndOutputs(QnnModelWrapper& qnn_m
     param_tensor_names.push_back(axis_param.GetParamTensorName());
     qnn_model_wrapper.AddParamWrapper(std::move(axis_param));
 
-    if (is_log_softmax) {
-      QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), QNN_OP_LOG_SOFTMAX_PARAM_BETA, beta_qnn_scalar);
-      param_tensor_names.push_back(beta_param.GetParamTensorName());
-      qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
-    }
+    QnnParamWrapper beta_param(node_unit.Index(), node_unit.Name(), beta_param_key, beta_qnn_scalar);
+    param_tensor_names.push_back(beta_param.GetParamTensorName());
+    qnn_model_wrapper.AddParamWrapper(std::move(beta_param));
 
     return ProcessOutputs(qnn_model_wrapper, node_unit,
                           std::move(input_names),
