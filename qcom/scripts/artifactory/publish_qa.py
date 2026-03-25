54ca5c7edbe8acb6eee0c34fccce62343679c979
Jeff Kilpatrick <jkilpat@qti.qualcomm.com>
2026-03-25 09:33:56 -0700

    Save nightly builds to Artifactory
    * Add zip archives and NuGet packages to the nightly build job.
    * Upload artifacts to artifactory for QA.

--- qcom/scripts/artifactory/publish_qa.py
+++ qcom/scripts/artifactory/publish_qa.py
@@ -5,37 +5,37 @@
 import argparse
 from pathlib import Path
 
-from artifactory import CiArtifactory, initialize_logging, valid_artifact_name
+from artifactory import QaArtifactory, initialize_logging, valid_artifact_name
 
 REPO_ROOT = Path(__file__).parent.parent.parent.parent.absolute()
 
 
-class PublishCiArtifact:
-    def __init__(self, name: str, build_dir: Path, src_pattern: str) -> None:
+class PublishQaArtifact:
+    def __init__(self, tag: str, build_dir: Path, src_pattern: str) -> None:
         self.__build_dir = build_dir
         self.__src_pattern = src_pattern
-        self.__client = CiArtifactory(name)
+        self.__client = QaArtifactory(tag)
 
     @property
     def destination(self) -> str:
         return f"{self.__client.artifact_root}/"
 
     def run(self) -> None:
-        self.__client.upload(self.__build_dir, self.__src_pattern, self.destination)
+        self.__client.upload(self.__build_dir, self.__src_pattern, self.destination, flat=True)
 
 
 if __name__ == "__main__":
-    initialize_logging("publish_ci.py")
+    initialize_logging("publish_qa.py")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--name", type=valid_artifact_name, required=True, help="Artifact name")
+    parser.add_argument("--tag", type=valid_artifact_name, required=True, help="QA tag")
     parser.add_argument("--src-root", type=Path, default=REPO_ROOT / "build", help="Root directory of artifacts")
     parser.add_argument("--src-pattern", type=str, required=True, help="Artifact file pattern, relative to SRC_ROOT.")
 
     args = parser.parse_args()
 
-    PublishCiArtifact(
-        args.name,
+    PublishQaArtifact(
+        args.tag,
         args.src_root,
         args.src_pattern,
     ).run()
