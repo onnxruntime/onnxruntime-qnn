73fc13cb509fcb44cd560cc1d6a778de167e03b7
qti-chuteng <chuteng@qti.qualcomm.com>
2026-03-26 01:01:06 +0800

    Update qnn_backend_manager.cc
    Signed-off-by: qti-chuteng <chuteng@qti.qualcomm.com>

--- onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
@@ -1412,7 +1412,7 @@ Ort::Status QnnBackendManager::SetupBackend(
   bool enable_htp_weight_sharing = false;
   if (share_ep_contexts && !load_from_cached_context) {
 #if defined(__aarch64__) || defined(_M_ARM64)
-#if QNN_API_VERSION_MAJOR > 2 || (QNN_API_VERSION_MAJOR == 2 && QNN_API_VERSION_MINOR >= 32)
+#if QNN_API_VERSION_MAJOR > 2 || (QNN_API_VERSION_MAJOR == 2 && QNN_API_VERSION_MINOR >= 34)
     enable_htp_weight_sharing = true;
 #else
     ORT_CXX_LOG(logger_,
