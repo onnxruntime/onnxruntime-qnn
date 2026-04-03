90eef15a970dc0b9b438c3ed1e472c5f17a37343
monumeen <monumeen@qti.qualcomm.com>
2026-04-03 11:39:47 +0530

    fix error

--- onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
@@ -1822,11 +1822,11 @@ void QnnBackendManager::CreateTimerThread(uint32_t htp_power_config_client_id) {
         timer_.reset();
         return;
       }
-      if (!timer_->Initialize(TimerCallback, timer_callback_arg_.get())) {
+      /*if (!timer_->Initialize(TimerCallback, timer_callback_arg_.get())) {
         ORT_CXX_LOG_PTR(logger_ptr_, ORT_LOGGING_LEVEL_VERBOSE, "Failed to create timer to set performance");
         timer_callback_arg_.reset();
         timer_.reset();
-      }
+      }*/
     } else {
       ORT_CXX_LOG_PTR(logger_ptr_, ORT_LOGGING_LEVEL_VERBOSE, "Failed: Timer is nullptr");
     }
@@ -1883,9 +1883,9 @@ Ort::Status QnnBackendManager::CreateHtpPowerCfgId(uint32_t device_id,
 
 Ort::Status QnnBackendManager::InitializePowerCfgId(uint32_t device_id, uint32_t core_id, uint32_t& htp_power_config_id) {
   auto status = CreateHtpPowerCfgId(device_id, core_id, htp_power_config_id);
-  /*if (status.IsOK()) {
+  if (status.IsOK()) {
     CreateTimerThread(htp_power_config_id);
-  }*/
+  }
   return status;
 }
 
