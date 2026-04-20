0aa18599aa820dbb9d3d3f7b8594693f2e568918
Yuduo Wu <yuduow@qti.qualcomm.com>
2026-04-20 16:02:56 -0700

    Address comments

--- onnxruntime/test/providers/qnn/identity_test.cc
+++ onnxruntime/test/providers/qnn/identity_test.cc
@@ -25,34 +25,34 @@ static GetTestModelFn BuildIdentityTestCase(const TestInputDef<DataType>& input_
 }
 
 template <typename QuantType>
-static GetTestQDQModelFn<QuantType> BuildQDQIdentityTestCase(const TestInputDef<float>& input_def) {
-  return [input_def](ModelTestBuilder& builder, std::vector<QuantParams<QuantType>>& output_qparams) {
+static GetTestQDQModelFn<QuantType> BuildQDQIdentityTestCase(const TestInputDef<float>& input_def,
+                                                             float output_scale_perturbation = 1.0f) {
+  return [input_def, output_scale_perturbation](ModelTestBuilder& builder,
+                                                std::vector<QuantParams<QuantType>>& output_qparams) {
     ORT_UNUSED_PARAMETER(output_qparams);
-
     MakeTestInput(builder, "input", input_def);
-
     const QuantParams<QuantType> input_qparams = GetTestInputQuantParams<QuantType>(input_def);
     const std::string input_qdq =
         AddQDQNodePair<QuantType>(builder, "qdq_in", "input",
                                   input_qparams.scale, input_qparams.zero_point);
-
     builder.AddNode("identity", "Identity", {input_qdq}, {"Y"}, kOnnxDomain);
-
+    const float output_scale = input_qparams.scale * output_scale_perturbation;
     AddQDQNodePairWithOutputAsGraphOutput<QuantType>(builder, "qdq_out", "Y",
-                                                     input_qparams.scale,
+                                                     output_scale,
                                                      input_qparams.zero_point);
   };
 }
 
 template <typename QuantType = uint8_t>
 static void RunIdentityQDQTest(const TestInputDef<float>& input_def,
-                               ExpectedEPNodeAssignment expected_ep_assignment) {
+                               ExpectedEPNodeAssignment expected_ep_assignment,
+                               float output_scale_perturbation = 1.0f) {
   ProviderOptions provider_options;
   provider_options["backend_type"] = "htp";
   provider_options["offload_graph_io_quantization"] = "0";
 
   TestQDQModelAccuracy(BuildIdentityTestCase<float>(input_def),
-                       BuildQDQIdentityTestCase<QuantType>(input_def),
+                       BuildQDQIdentityTestCase<QuantType>(input_def, output_scale_perturbation),
                        provider_options,
                        18,
                        expected_ep_assignment);
@@ -73,13 +73,15 @@ static void RunIdentityNonQDQOnHTP(const TestInputDef<DataType>& input_def,
 }
 
 TEST_F(QnnHTPBackendTests, IdentityU8) {
+  // Use slightly different output scale (1.0001x) to exercise
+  // SetOutputQParamEqualToInputIfNearlyEqual snapping output qparams to input.
   RunIdentityQDQTest(TestInputDef<float>({1, 3, 4, 4}, false, 0.0f, 1.0f),
-                     ExpectedEPNodeAssignment::All);
+                     ExpectedEPNodeAssignment::All, /*output_scale_perturbation=*/1.0001f);
 }
 
 TEST_F(QnnHTPBackendTests, IdentityF32) {
   RunIdentityNonQDQOnHTP<float>(TestInputDef<float>({1, 3, 4, 4}, false, 0.0f, 10.0f),
-                                ExpectedEPNodeAssignment::All, 5e-3f);
+                                ExpectedEPNodeAssignment::All);
 }
 
 TEST_F(QnnHTPBackendTests, IdentityI32) {
@@ -89,7 +91,7 @@ TEST_F(QnnHTPBackendTests, IdentityI32) {
 
 TEST_F(QnnHTPBackendTests, IdentityRank1F32) {
   RunIdentityNonQDQOnHTP<float>(TestInputDef<float>({16}, false, 0.0f, 10.0f),
-                                ExpectedEPNodeAssignment::All, 5e-3f);
+                                ExpectedEPNodeAssignment::All);
 }
 
 #endif  // defined(__aarch64__) || defined(_M_ARM64) || defined(__linux__)
