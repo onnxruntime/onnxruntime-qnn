c9570032f7ae43e5da83aa2faa6e82a851adc4fa
Badri Narayanan <mbadnara@qti.qualcomm.com>
2026-04-21 21:09:23 -0700

    [QNN EP] Skip Max Pool Rank 3 test for Linux

--- onnxruntime/test/providers/qnn/maxpool_test.cc
+++ onnxruntime/test/providers/qnn/maxpool_test.cc
@@ -109,9 +109,12 @@ TEST_F(QnnCPUBackendTests, MaxPool_Global) {
 }
 
 TEST_F(QnnCPUBackendTests, MaxPool_Rank3) {
+  // Skip on Linux v68 architecture
+  #if defined(__linux__) || defined(__APPLE__)
   if (QnnHTPBackendTests::ShouldSkipIfHtpArchIsLessThanOrEqualTo(QNN_HTP_DEVICE_ARCH_V68)) {
-    GTEST_SKIP() << "Skipping this test (arch > V68).";
+    GTEST_SKIP() << "Test not supported on Linux v68 architecture.";
   }
+  #endif
   RunPoolOpTest("MaxPool",
                 TestInputDef<float>({1, 16, 120}, false, -10.0f, 10.0f),  // Dynamic input with range [-10, 10]
                 {test::MakeAttribute("kernel_shape", std::vector<int64_t>{3}),
