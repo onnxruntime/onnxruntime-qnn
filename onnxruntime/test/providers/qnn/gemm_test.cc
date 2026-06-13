114c097b21737030c1b28697205078d0138b6ac4
Badri Narayanan <mbadnara@qti.qualcomm.com>
2026-06-12 19:50:00 -0700

    [QNN EP] Fix test case failures due to revert

--- onnxruntime/test/providers/qnn/gemm_test.cc
+++ onnxruntime/test/providers/qnn/gemm_test.cc
@@ -273,8 +273,18 @@ inline GetTestQDQModelFn<InputAQType> BuildQDQGemmTestCase(const std::vector<Tes
     gemm_inputs.push_back(a_qdq);
     gemm_inputs.push_back(b_qdq);
 
+    // Check if beta=0.0 (bias is ignored)
+    float beta = 1.0f;
+    for (const auto& attr : attrs) {
+      if (attr.name() == "beta") {
+        beta = attr.f();
+        break;
+      }
+    }
+
     // Bias (optional): int32 -> DQ
-    if (num_inputs == 3) {
+    // Skip bias when beta=0.0 since it's not used by the Gemm op
+    if (num_inputs == 3 && beta != 0.0f) {
       const std::string bias_dq = MakeTestQDQBiasInput(
           builder, "C", input_defs[2], a_qparams.scale * b_qparams.scale, use_contrib_qdq);
       gemm_inputs.push_back(bias_dq);
