6e39b41e5d2d782a980274484cec816cc3b03313
Nischay Ram Mamidi <niscmami@qti.qualcomm.com>
2026-07-01 10:45:17 -0700

    Use float instead of undefined dtype

--- onnxruntime/core/providers/qnn/builder/opbuilder/lstm_op_builder.cc
+++ onnxruntime/core/providers/qnn/builder/opbuilder/lstm_op_builder.cc
@@ -386,7 +386,7 @@ Ort::Status LSTMOpBuilder::AddUnidirectionLSTM(QnnModelWrapper& qnn_model_wrappe
 
   // Common LSTM cell inputs
   const std::string null_tensor_name = "null_tensor";
-  QnnTensorWrapper null_tensor_wrapper(null_tensor_name, QNN_TENSOR_TYPE_NULL, QNN_DATATYPE_UNDEFINED,
+  QnnTensorWrapper null_tensor_wrapper(null_tensor_name, QNN_TENSOR_TYPE_NULL, QNN_DATATYPE_FLOAT_32,
                                        QnnQuantParamsWrapper(), std::vector<uint32_t>{0});
 
   qnn_model_wrapper.AddTensorWrapper(std::move(null_tensor_wrapper));
